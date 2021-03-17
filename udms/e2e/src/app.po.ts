abe8f08ba106692fff7f35612731d0223d7f990a
Haoli Du <haolidu@google.com>
2021-03-17 05:27:22 +0000

    fix existing e2e tests. New tests are still pending.

--- udms/e2e/src/app.po.ts
+++ udms/e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+    return element(by.css('app-root mat-toolbar .title')).getText();
   }
 }
