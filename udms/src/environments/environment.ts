8da032d0e751a17e22f32e542e54118d4cf250db
Anurag Porripireddi <anuragprdi@google.com>
2021-07-16 05:26:07 -0700

    Disable emulators

--- udms/src/environments/environment.ts
+++ udms/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  useEmulators: true,
+  useEmulators: false,
   firebase: {
     apiKey: 'AIzaSyANL8VhKgHhD71lMNw6MUa_CRlpQs6Z0Pc',
     authDomain: 'bos-daq-testing.firebaseapp.com',
