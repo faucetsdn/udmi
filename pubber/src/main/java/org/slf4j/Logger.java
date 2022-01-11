df45021236701f91dddd234433fe346c83189317
Trevor Pering <peringknife@google.com>
2022-01-11 06:15:36 -0800

    Adding docker build

---
+++ pubber/src/main/java/org/slf4j/Logger.java
@@ -0,0 +1,23 @@
+package org.slf4j;
+
+public class Logger {
+
+  public void error(String fatal_error_during_execution, Exception e) {
+  }
+
+  public void error(String forcing_termination) {
+  }
+
+  public void warn(String s) {
+  }
+
+  public void warn(String mqtt_connection_lost, Throwable cause) {
+  }
+
+  public void info(String done_with_main) {
+  }
+
+  public void debug(String s) {
+  }
+
+}
---
+++ pubber/src/main/java/org/slf4j/LoggerFactory.java
@@ -0,0 +1,8 @@
+package org.slf4j;
+
+public class LoggerFactory {
+
+  public static Logger getLogger(Class<?> clazz) {
+    return null;
+  }
+}
