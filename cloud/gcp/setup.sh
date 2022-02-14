55970ff0cdbd8540bcd94c8c715ac1cd7b37327c
Trevor Pering <peringknife@google.com>
2022-02-14 13:25:39 -0800

    Updating with creds

--- cloud/gcp/setup.sh
+++ cloud/gcp/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash -e
 
+CRED_FILE=auth/credentials.json
+
 echo export GCP_PROJECT_NAME=${GCP_PROJECT_NAME:?}
 echo export GCP_PROJECT_ID=${GCP_PROJECT_ID:?}
 echo export GCP_PROJECT_REGION=${GCP_PROJECT_REGION:?}
 echo export UDMI_SITE_NAME=${UDMI_SITE_NAME:?}
 echo export UDMI_SITE_REGION=${UDMI_SITE_REGION:?}
 echo export UDMI_SITE_GROUP=${UDMI_SITE_GROUP:?}
+echo export GCP_PROJECT_CREDS=${GCP_BASE64_CREDS:?}
+
+if [[ -f $CRED_FILE ]]; then
+    echo Cowardly refusing to overwrite existing $CRED_FILE
+    false
+else
+    base64 -d <<< "${GCP_PROJECT_CREDS}" > $CRED_FILE
+fi
 
 sed -E < main.tf.template > main.tf \
     -e "s/@GCP_PROJECT_ID@/${GCP_PROJECT_ID}/"
