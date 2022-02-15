63fe2afd1b245091e32a75c1520e598903da8ef4
Trevor Pering <peringknife@google.com>
2022-02-15 10:47:51 -0800

    Terraform config

--- cloud/gcp/setup.sh
+++ cloud/gcp/setup.sh
@@ -1,33 +1,43 @@
 #!/bin/bash -e
 
 CRED_FILE=auth/credentials.json
-
-echo export GCP_PROJECT_NAME=${GCP_PROJECT_NAME:?}
-echo export GCP_PROJECT_ID=${GCP_PROJECT_ID:?}
-echo export GCP_PROJECT_REGION=${GCP_PROJECT_REGION:?}
-echo export GCP_PROJECT_GROUP=${GCP_PROJECT_GROUP:?}
-echo export UDMI_SITE_NAME=${UDMI_SITE_NAME:?}
-echo export UDMI_SITE_REGION=${UDMI_SITE_REGION:?}
-echo export UDMI_SITE_GROUP=${UDMI_SITE_GROUP:?}
-echo export GCP_PROJECT_CREDS=${GCP_BASE64_CREDS:?}
+CONFIG_FILE=config.out
 
 if [[ -f $CRED_FILE ]]; then
     echo Cowardly refusing to overwrite existing $CRED_FILE
-    false
 else
-    base64 -d <<< "${GCP_PROJECT_CREDS}" > $CRED_FILE
+    base64 -d <<< "${GCP_PROJECT_CREDS:?}" > $CRED_FILE
 fi
 
+cat <<EOF > $CONFIG_FILE
+export GCP_PROJECT_NAME=${GCP_PROJECT_NAME:?}
+export GCP_PROJECT_ID=${GCP_PROJECT_ID:?}
+export GCP_PROJECT_REGION=${GCP_PROJECT_REGION:?}
+export GCP_PROJECT_GROUP=${GCP_PROJECT_GROUP:?}
+export GCP_PROJECT_CREDS=`base64 -w 0 $CRED_FILE`
+export UDMI_SITE_NAME=${UDMI_SITE_NAME:?}
+export UDMI_SITE_REGION=${UDMI_SITE_REGION:?}
+export UDMI_SITE_GROUP=${UDMI_SITE_GROUP:?}
+EOF
+
+echo Config saved to $CONFIG_FILE
+sha256sum $CONFIG_FILE
+
+echo Creating main.tf
 sed -E < main.tf.template > main.tf \
     -e "s/@GCP_PROJECT_ID@/${GCP_PROJECT_ID}/"
 
+echo Creating udmi-sites.tf
 sed -E < udmi-sites.tf.template > udmi-sites.tf \
     -e "s/@UDMI_SITE_NAME@/${UDMI_SITE_NAME}/" \
     -e "s/@UDMI_SITE_REGION@/${UDMI_SITE_REGION}/" \
     -e "s/@UDMI_SITE_GROUP@/${UDMI_SITE_GROUP}/"
 
+echo Creating terraform.tfvars
 sed -E < terraform.tfvars.template > terraform.tfvars \
     -e "s/@GCP_PROJECT_NAME@/${GCP_PROJECT_NAME}/g" \
     -e "s/@GCP_PROJECT_ID@/${GCP_PROJECT_ID}/" \
     -e "s/@GCP_PROJECT_REGION@/${GCP_PROJECT_REGION}/" \
     -e "s/@GCP_PROJECT_GROUP@/${GCP_RPOJECT_GROUP}/"
+
+echo Done with terraform setup.
