8ca007d80511c7b2970c146b93b15fd341ec8aa7
slevert <stephane.levert@buildingsiot.com>
2022-02-24 17:24:40 -0500

    Adding source

---
+++ udmd/api/src/app.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server";
+import dataSources from './server/datasources';
+import { typeDefs } from './server/schema';
+import { resolvers } from './server/resolvers';
+import {  logger } from './common/logger';
+import { getDefaultContextProcessor }  from './common/context';
+
+(async () => {
+
+  // required context processor
+  const context = await getDefaultContextProcessor();
+
+  // server initialization
+  const server: ApolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    dataSources: dataSources()
+  });
+
+  // start our server
+  server.listen(4001).then(({ url }) => {
+    logger.info(`🎙  Universal Device Management Dashboard API is ready to talk: ${url}`);
+  });
+})().catch((err) => logger.error(err.stack));
+
