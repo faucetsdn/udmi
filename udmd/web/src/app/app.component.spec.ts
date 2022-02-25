66c5375a4090d330df0709f6ba15440080c13d25
Marc Spratt <marc.spratt@kodaro.com>
2022-02-25 14:19:24 -0500

    renamed dmc to udmd

--- udmd/web/src/app/app.component.spec.ts
+++ udmd/web/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dmc-web'`, () => {
+  it(`should have as title 'udmd-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dmc-web');
+    expect(app.title).toEqual('udmd-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dmc-web app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('udmd-web app is running!');
   });
 });
