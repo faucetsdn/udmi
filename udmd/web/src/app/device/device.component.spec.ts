fcd3624f77738efc99507397a742007e6f5d415e
Marc Spratt <marc.spratt@kodaro.com>
2022-03-11 15:04:21 -0500

    added tests

--- udmd/web/src/app/device/device.component.spec.ts
+++ udmd/web/src/app/device/device.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { DeviceComponent } from './device.component';
 import { DeviceModule } from './device.module';
 
 describe('DeviceComponent', () => {
   let component: DeviceComponent;
   let fixture: ComponentFixture<DeviceComponent>;
+  let deviceId: string = 'device-id-1';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [DeviceModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              params: {
+                id: deviceId,
+              },
+            },
+          },
+        },
+      ],
     }).compileComponents();
   });
 
@@ -20,5 +34,9 @@ describe('DeviceComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should store the device id in memory', () => {
+    expect(component.deviceId).toEqual(deviceId);
   });
 });
