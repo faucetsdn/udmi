8cea2be1294123cf76d9950460b9782b2d7b65d2
Marc Spratt <marc.spratt@kodaro.com>
2022-03-15 14:39:00 -0400

    added setup for device summary

--- udmd/web/src/app/device/device.component.ts
+++ udmd/web/src/app/device/device.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Device } from '../devices/devices';
 
 @Component({
   templateUrl: './device.component.html',
   styleUrls: ['./device.component.scss'],
 })
 export class DeviceComponent implements OnInit {
-  deviceId: string | null = null;
+  fields: (keyof Device)[] = ['name', 'make', 'model', 'site', 'section', 'operational', 'tags'];
+  device!: Device;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.deviceId = this.route.snapshot.params['id'];
+    const deviceId = this.route.snapshot.params['id'];
+
+    // TODO:: make call to get device by id
+    this.device = {
+      id: '123',
+      name: 'Name2',
+      make: 'TEst',
+      model: 'Model X',
+      site: 'LA',
+      section: 'Bay 4',
+      operational: true,
+      tags: [],
+    };
   }
 }
--- udmd/web/src/app/device/device.module.ts
+++ udmd/web/src/app/device/device.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { DeviceComponent } from './device.component';
 import { DeviceRoutingModule } from './device-routing.module';
 import { BreadcrumbModule } from '../breadcrumb/breadcrumb.module';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { PipesModule } from '../common/pipes/pipes.module';
 
 @NgModule({
   declarations: [DeviceComponent],
-  imports: [DeviceRoutingModule, BreadcrumbModule],
+  imports: [DeviceRoutingModule, BreadcrumbModule, MatCardModule, CommonModule, PipesModule],
 })
 export class DeviceModule {}
