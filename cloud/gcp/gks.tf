4d587b7ca308f5d77cf599c9ceca3e9f2644a78d
Narmadha Koti <narmadha.koti@buildingsiot.com>
2022-03-17 13:47:57 -0400

    configured vaiables

--- cloud/gcp/gks.tf
+++ cloud/gcp/gks.tf
@@ -1,21 +1,33 @@
 variable "gke_num_nodes" {
     type = number 
-    default = "1"
+    default = ""
     description = "number of gke nodes"
 }
+variable "node_locations" {
+  type = list(string)
+  default = ""
+  description = "The list of zones in which the cluster's nodes are located"
+}
+
+variable "initial_node_count" {
+  type = number
+  default = 1
+  description = "The number of nodes to create in this cluster"
+}
+
 
 #GKE CLUSTER
 resource "google_container_cluster" "biot" {
     name     = "${var.gcp_project_id}-gke"
     location = var.gcp_region
-    node_locations = ["us-central1-b"]
+    node_locations = var.node_locations
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = var.initial_node_count
   
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
--- cloud/gcp/vpc.tf
+++ cloud/gcp/vpc.tf
@@ -3,7 +3,11 @@ variable "gcp_vpc_name" {
     default = "udmi"
     description = "vpc name"
 }
-
+variable "ip_cidr_range" {
+  type = string
+  default = "10.10.0.0/24"
+  description = "The range of internal addresses that are owned by this subnetwork"
+}
 # VPC
 resource "google_compute_network" "vpc" {
   name                    = "${var.gcp_vpc_name}-vpc"
@@ -15,5 +19,5 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.gcp_vpc_name}-subnet"
   region        = var.gcp_region
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = var.ip_cidr_range
 }
