47cdcadd509dca9a06771b7674bb330090f37b52
Narmadha Koti <narmadha.koti@buildingsiot.com>
2022-03-22 11:08:20 -0400

    fixed lab name issue

--- cloud/gcp/gks.tf
+++ cloud/gcp/gks.tf
@@ -1,48 +1,14 @@
-variable "gke_num_nodes" {
-    type = number 
-    description = "number of gke nodes"
-}
-variable "node_locations" {
-  type = list(string)
-  description = "The list of zones in which the cluster's nodes are located"
-}
-
-variable "initial_node_count" {
-  type = number
-  default = 1
-  description = "The number of nodes to create in this cluster"
-}
-
-variable "gke_cluster_name" {
-  type = string
-  description = "gke cluster name"
-}
-
-variable "node_pool_name" {
-  type = string
-  description = "The name of the node pool"
-}
-variable "gke_cluster_location" {
-  type = string
-  description = "The location (region or zone) of the cluster"
-}
-variable "machine_type" {
-  type = string
-  description = "Type of machine"
-}
-
 #GKE CLUSTER
 resource "google_container_cluster" "udmi" {
     name     = var.gke_cluster_name
     location = var.gke_cluster_location
-    node_locations = var.node_locations
+    node_locations = var.gke_node_locations
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   
-  remove_default_node_pool = true
-  initial_node_count       = var.initial_node_count
+  initial_node_count       = var.gke_initial_node_count
   
   network    = var.create_vpc ? google_compute_network.vpc[0].name : null
   subnetwork = var.create_vpc ? google_compute_subnetwork.subnet[0].name : null
@@ -50,7 +16,7 @@ resource "google_container_cluster" "udmi" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "node_pool" {
-  name       = var.node_pool_name
+  name       = var.gke_node_pool_name
   location   = var.gke_cluster_location
   cluster    = google_container_cluster.udmi.name
   node_count = var.gke_num_nodes
@@ -58,7 +24,7 @@ resource "google_container_node_pool" "node_pool" {
   node_config {
 
     # preemptible  = true
-    machine_type = var.machine_type
+    machine_type = var.gke_machine_type
     metadata = {
       disable-legacy-endpoints = "true"
     }
--- cloud/gcp/variables.tf
+++ cloud/gcp/variables.tf
@@ -42,3 +42,60 @@ variable "log_level" {
   type        = string
   description = "Log level"
 }
+
+##GKE variables##
+variable "gke_num_nodes" {
+    type = number
+    default = 1
+    description = "number of gke nodes"
+}
+variable "gke_node_locations" {
+  type = list(string)
+  default = []
+  description = "The list of zones in which the cluster's nodes are located"
+}
+
+variable "gke_initial_node_count" {
+  type = number
+  default = 0
+  description = "The number of nodes to create in this cluster"
+}
+
+variable "gke_cluster_name" {
+  type = string
+  default = "udmi"
+  description = "gke cluster name"
+}
+
+variable "gke_node_pool_name" {
+  type = string
+  default = "udmi"
+  description = "The name of the node pool"
+}
+variable "gke_cluster_location" {
+  type = string
+  default = "us-central1-f"
+  description = "The location (region or zone) of the cluster"
+}
+variable "gke_machine_type" {
+  type = string
+  default = "e2-standard-2"
+  description = "Type of machine"
+}
+
+##vpc variables##
+variable "gcp_vpc_name" {
+    type = string
+    default = "udmi"
+    description = "vpc name"
+}
+variable "ip_cidr_range" {
+  type = string
+  default = "10.10.0.0/24"
+  description = "The range of internal addresses that are owned by this subnetwork"
+}
+variable "create_vpc" {
+  type = bool
+  default = false
+  description = "we can use default vpc or new vpc"
+}
