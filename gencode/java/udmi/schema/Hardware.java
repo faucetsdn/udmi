ba9a2f8fcdb72410ba3d7562467b8353ab70a736
Trevor Pering <peringknife@google.com>
2022-03-22 08:01:36 -0700

    Gencode

---
+++ gencode/java/udmi/schema/Hardware.java
@@ -0,0 +1,34 @@
+
+package udmi.schema;
+
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+
+})
+@Generated("jsonschema2pojo")
+public class Hardware {
+
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Hardware) == false) {
+            return false;
+        }
+        Hardware rhs = ((Hardware) other);
+        return true;
+    }
+
+}
