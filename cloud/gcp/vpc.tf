e0b07f2d3bf571ac9245dc6ff92d0a88f8967b38
naveen30 <naveen.lakshmi@buildingsiot.com>
2022-03-22 08:50:46 -0400

    updated function

--- cloud/gcp/vpc.tf
+++ cloud/gcp/vpc.tf
@@ -8,16 +8,23 @@ variable "ip_cidr_range" {
   default = "10.10.0.0/24"
   description = "The range of internal addresses that are owned by this subnetwork"
 }
+variable "create_vpc" {
+  type = bool
+  default = false
+  description = "we can use default vpc or new vpc"
+}
 # VPC
 resource "google_compute_network" "vpc" {
+  count                   = var.create_vpc ? 1 : 0
   name                    = "${var.gcp_vpc_name}-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
+  count         = var.create_vpc ? 1 : 0
   name          = "${var.gcp_vpc_name}-subnet"
   region        = var.gcp_region
-  network       = google_compute_network.vpc.name
+  network       = google_compute_network.vpc[count.index].name
   ip_cidr_range = var.ip_cidr_range
 }
