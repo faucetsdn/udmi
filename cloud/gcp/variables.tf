9f3489e8094e7e0080dc94d92e9b0b4d54905eb9
naveen30 <naveen.lakshmi@buildingsiot.com>
2022-03-23 10:50:11 -0400

    fuvariable to tfvars

--- cloud/gcp/variables.tf
+++ cloud/gcp/variables.tf
@@ -103,37 +103,25 @@ variable "create_vpc" {
 ## function variables 
 variable "function_name" {
     type = string
-    default = "transform-EventHandler"
     description = "functions name"
 }
 variable "function_memory" {
     type = number
-    default = 256
     description = "The amount of memory in megabytes allotted for the function to use."
 }
 variable "function_runtime" {
     type = string 
-    default = "nodejs14"
     description = "The runtime in which the function will be executed."
 }
 variable "function_timeout" {
     type = number
-    default = 20
     description = "The amount of time in seconds allotted for the execution of the function."
 }
 variable "function_entry_point" {
      type = string
-     default = "udmi-EventHandler"
      description = "The name of a method in the function source which will be invoked when the function is executed."
 }
 variable "function_environment_variables" {
   type        = map(string)
-  default     = {
-    MONGO_PROTOCOL= "mongodb+srv"
-    MONGO_USER= ""
-    MONGO_PWD= ""
-    MONGO_HOST= "preprod.imvgx.mongodb.net/udmi?retryWrites=true&w=majority"
-    MONGO_DB= "udmi"
-  }
   description = "A set of key/value environment variable pairs to assign to the function."
 }
