47c2785110917c350f0a6554b30c81df777696f5
Trevor Pering <peringknife@google.com>
2022-03-23 21:22:12 -0700

    Linty

--- validator/src/main/java/com/google/daq/mqtt/validator/SequenceTestRunner.java
+++ validator/src/main/java/com/google/daq/mqtt/validator/SequenceTestRunner.java
@@ -7,6 +7,9 @@ import org.junit.runner.Request;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+/**
+ * Custom test runner that can execute a specific method to test.
+ */
 public class SequenceTestRunner {
 
   private static final String INITIALIZATION_ERROR_PREFIX = "initializationError(org.junit.";
@@ -14,6 +17,13 @@ public class SequenceTestRunner {
   private static final int EXIST_STATUS_FAILURE = 1;
   private static final int EXIT_STATUS_NO_TESTS = 2;
 
+  /**
+   * Thundercats go!
+   * 
+   * @param args Test classes/method to test.
+   *
+   * @throws ClassNotFoundException For bad test class
+   */
   public static void main(String... args) throws ClassNotFoundException {
     int successes = 0;
     List<Failure> failures = new ArrayList<>();
@@ -51,7 +61,7 @@ public class SequenceTestRunner {
   }
 
   private static boolean hasActualTestResults(Result result) {
-    return (result.getFailures().size() != 1 ||
-        !result.getFailures().get(0).toString().startsWith(INITIALIZATION_ERROR_PREFIX));
+    return (result.getFailures().size() != 1
+        || !result.getFailures().get(0).toString().startsWith(INITIALIZATION_ERROR_PREFIX));
   }
 }
