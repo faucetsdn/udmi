ae12797783de048580464b5e59bdf0bab30139e9
Trevor Pering <peringknife@google.com>
2022-04-01 08:55:36 -0700

    Allow processing raw streams

--- validator/src/main/java/com/google/daq/mqtt/validator/Validator.java
+++ validator/src/main/java/com/google/daq/mqtt/validator/Validator.java
@@ -450,7 +450,7 @@ public class Validator {
   private boolean shouldValidateMessage(Map<String, String> attributes) {
     String subType = attributes.get("subType");
     String subFolder = attributes.get("subFolder");
-    return "event".equals(subType) || "update".equals(subType);
+    return subType == null || "event".equals(subType) || "update".equals(subType);
   }
 
   private File prepareDeviceOutDir(
