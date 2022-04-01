194c91a80a73c5717b4372ddba188858027ebcda
Trevor <grafnu@users.noreply.github.com>
2022-03-31 21:13:44 -0700

    Make gateway error structure more flexible and single test runner (#277)

--- validator/src/main/java/com/google/daq/mqtt/validator/CleanDateFormat.java
+++ validator/src/main/java/com/google/daq/mqtt/validator/CleanDateFormat.java
@@ -5,18 +5,37 @@ import java.text.ParseException;
 import java.util.Date;
 
 /**
- * Date format that strips out the ms to keep compatibility with string representations that
- * don't include allllllll the seconds. This basically makes .equals() work.
+ * Date format that strips out the ms to keep compatibility with string representations that don't
+ * include allllllll the seconds. This basically makes .equals() work.
  */
 public class CleanDateFormat extends ISO8601DateFormat {
 
-  static Date cleanDate(Date parsedDate) {
-    parsedDate.setTime(parsedDate.getTime() - parsedDate.getTime() % 1000);
+  /**
+   * Clean a date object (remove ms).
+   *
+   * @param parsedDate date to clean
+   * @return cleaned date
+   */
+  public static Date cleanDate(Date parsedDate) {
+    if (parsedDate != null) {
+      parsedDate.setTime(parsedDate.getTime() - parsedDate.getTime() % 1000);
+    }
     return parsedDate;
   }
 
   static Date cleanDate() {
     return cleanDate(new Date());
+  }
+
+  /**
+   * Check if two dates are equal after cleaning them up.
+   *
+   * @param dateBase   base date
+   * @param dateTarget compare against
+   * @return if the dates are equal (after cleaning)
+   */
+  public static boolean dateEquals(Date dateBase, Date dateTarget) {
+    return cleanDate(dateBase).equals(cleanDate(dateTarget));
   }
 
   @Override
