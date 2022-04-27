a01a0700f3da86d3608d3875ef2c4d83afafef13
Marc Spratt <marc.spratt@kodaro.com>
2022-04-27 12:11:06 -0400

    #961-fixed typos

--- udmif/web/prod/docker-entrypoint.sh
+++ udmif/web/prod/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
-# Replace variables in the env 'template' file with Docker runtime environment variables.
-# Output a regular env.conf file in the nginx's web directory with the variables replaced.
+# Replace variables in the env 'template' file with Docker
+# runtime environment variables. Output a renamed env.js file.
 envsubst '${GOOGLE_CLIENT_ID} ${API_URI}' < env.template.js > env.js
 
 exec "$@"
