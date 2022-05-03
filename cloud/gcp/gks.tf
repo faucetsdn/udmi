b12433f58c1154937a7c14133d993a601e53a5f2
naveen30 <naveen.lakshmi@buildingsiot.com>
2022-05-03 14:01:18 -0400

    deleted unwanted files

--- cloud/gcp/gks.tf
+++ cloud/gcp/gks.tf
@@ -1,7 +1,7 @@
 resource "google_service_account" "gke" {
   account_id   = "gkeuser"
   display_name = "GKE Service Account"
-  project = var.gcp_project_id
+  project      = var.gcp_project_id
 }
 resource "google_project_iam_member" "gke_gcr_binding" {
   project = var.gcp_project_id
@@ -10,9 +10,9 @@ resource "google_project_iam_member" "gke_gcr_binding" {
 }
 #GKE CLUSTER
 resource "google_container_cluster" "udmi" {
-    project = var.gcp_project_id
-    name     = var.gke_cluster_name
-    location = var.gke_cluster_location
+    project        = var.gcp_project_id
+    name           = var.gke_cluster_name
+    location       = var.gke_cluster_location
     node_locations = var.gke_node_locations
 
   # We can't create a cluster with no node pool defined, but we want to only use
@@ -20,16 +20,14 @@ resource "google_container_cluster" "udmi" {
   # node pool and immediately delete it.
 
   remove_default_node_pool = true
-
   initial_node_count       = var.gke_initial_node_count
-  
   network    = var.create_vpc ? google_compute_network.vpc[0].name : null
   subnetwork = var.create_vpc ? google_compute_subnetwork.subnet[0].name : null
 }
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "node_pool" {
-  project = var.gcp_project_id
+  project    = var.gcp_project_id
   name       = var.gke_node_pool_name
   location   = var.gke_cluster_location
   cluster    = google_container_cluster.udmi.name
@@ -55,7 +53,7 @@ resource "google_container_node_pool" "node_pool" {
 
 #This resource creates static IP
 resource "google_compute_global_address" "udmi_global_address"{
- name = "udmi-global-address"
+ name    = "udmi-global-address"
  project = var.gcp_project_id
 }
 
@@ -63,22 +61,22 @@ resource "google_compute_global_address" "udmi_global_address"{
 resource "google_dns_managed_zone" "udmi_dns_zone" {
   name     = var.gcp_project_name
   dns_name = var.dns_name
-  project = var.gcp_project_id
+  project  = var.gcp_project_id
 }
 
 #This resource creates A record in cloud dns
 resource "google_dns_record_set" "dns_record" {
-  project = var.gcp_project_id
+  project      = var.gcp_project_id
   managed_zone = var.gcp_project_name
-  name = "*.${var.dns_name}"
-  type = "A"
-  ttl = 300
-  rrdatas = ["${google_compute_global_address.udmi_global_address.address}"]
+  name         = "*.${var.dns_name}"
+  type         = "A"
+  ttl          = 300
+  rrdatas      = ["${google_compute_global_address.udmi_global_address.address}"]
 }
 
 #This resouce creates ssl certs
 resource "google_compute_managed_ssl_certificate" "udmi_ssl_certs" {
-  name = "udmi-ssl"
+  name    = "udmi-ssl"
   project = var.gcp_project_id
   managed {
     domains = var.ssl_domains
