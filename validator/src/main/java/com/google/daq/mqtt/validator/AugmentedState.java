d1090d3e366124b1a3d6a67cd1edf79166325813
Trevor Pering <peringknife@google.com>
2022-06-08 10:42:42 -0700

    Linty fixes

--- validator/src/main/java/com/google/daq/mqtt/validator/AugmentedState.java
+++ validator/src/main/java/com/google/daq/mqtt/validator/AugmentedState.java
@@ -2,6 +2,9 @@ package com.google.daq.mqtt.validator;
 
 import udmi.schema.State;
 
+/**
+ * Special state wrapper class including extra fields for side-band data from backend functions.
+ */
 public class AugmentedState extends State {
   // Extra field indicating if the last config has been ackd by the device.
   public String configAcked;
