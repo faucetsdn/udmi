d420f7acc1b9b28ecee9db01db2dcf20c447f95c
Marc Spratt <marc.spratt@kodaro.com>
2022-06-10 09:35:13 -0400

    #1179-removed state from devices service

--- udmif/web/src/app/devices/devices.component.ts
+++ udmif/web/src/app/devices/devices.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { Sort } from '@angular/material/sort';
 import { SearchFilterItem } from '../search-filter/search-filter';
 import { Device, DeviceModel } from '../device/device';
-import { SortOptions } from './devices';
+import { DevicesQueryResponse, DevicesQueryVariables, SortOptions } from './devices';
 import { DevicesService } from './devices.service';
+import { QueryRef } from 'apollo-angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss'],
 })
-export class DevicesComponent implements OnInit {
+export class DevicesComponent implements OnInit, OnDestroy {
+  private devicesSubscription!: Subscription;
+  private devicesQuery!: QueryRef<DevicesQueryResponse, DevicesQueryVariables>;
   displayedColumns: (keyof DeviceModel)[] = [
     'name',
     'make',
@@ -21,7 +25,6 @@ export class DevicesComponent implements OnInit {
     'operational',
     'tags',
   ];
-  loading: boolean = true;
   devices: Device[] = [];
   totalCount: number = 0;
   totalFilteredCount: number = 0;
@@ -41,21 +44,24 @@ export class DevicesComponent implements OnInit {
   constructor(private devicesService: DevicesService) {}
 
   ngOnInit(): void {
-    this.devicesService.getDevices(0, this.pageSize).subscribe(({ data, loading }) => {
-      this.loading = loading;
+    this.devicesQuery = this.devicesService.getDevices(0, this.pageSize); // start off on first page, i.e. offset 0
+
+    this.devicesSubscription = this.devicesQuery.valueChanges.subscribe(({ data }) => {
       this.devices = data.devices?.devices ?? [];
       this.totalCount = data.devices?.totalCount ?? 0;
       this.totalFilteredCount = data.devices?.totalFilteredCount ?? 0;
-    }); // start off on first page, i.e. offset 0
+    });
+  }
 
-    //TODO:: Keep observable alive after error.
+  ngOnDestroy() {
+    this.devicesSubscription.unsubscribe(); // cleanup
   }
 
   pageChanged(e: PageEvent): void {
     this.pageSize = e.pageSize;
     this.currentPage = e.pageIndex;
 
-    this.devicesService.fetchMore(this.currentPage * this.pageSize, this.pageSize, this.sortOptions, this.filter);
+    this._refetch(this.currentPage * this.pageSize);
   }
 
   sortData(e: Sort): void {
@@ -66,12 +72,24 @@ export class DevicesComponent implements OnInit {
         }
       : undefined; // don't send sortOptions field if no direction
 
-    this.devicesService.fetchMore(0, this.pageSize, this.sortOptions, this.filter); // go back to first page, i.e. offset 0
+    this._refetch();
   }
 
   filterData = (filters: SearchFilterItem[]): void => {
+    // arrow to hold onto this
     this.filter = filters.length ? JSON.stringify(filters) : undefined; // don't send filter field if no filter
 
-    this.devicesService.fetchMore(0, this.pageSize, this.sortOptions, this.filter); // go back to first page, i.e. offset 0
+    this._refetch();
   };
+
+  private _refetch(offset: number = 0): void {
+    this.devicesQuery.refetch({
+      searchOptions: {
+        offset,
+        batchSize: this.pageSize,
+        sortOptions: this.sortOptions,
+        filter: this.filter,
+      },
+    });
+  }
 }
--- udmif/web/src/app/devices/devices.service.ts
+++ udmif/web/src/app/devices/devices.service.ts
@@ -26,14 +26,11 @@ import {
   SortOptions,
 } from './devices';
 import { map, Observable } from 'rxjs';
-import { ApolloQueryResult } from '@apollo/client/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DevicesService {
-  devicesQuery!: QueryRef<DevicesQueryResponse, DevicesQueryVariables>;
-
   constructor(private apollo: Apollo) {}
 
   getDevices(
@@ -41,11 +38,11 @@ export class DevicesService {
     batchSize: number = 10,
     sortOptions?: SortOptions,
     filter?: string
-  ): Observable<ApolloQueryResult<DevicesQueryResponse>> {
-    this.devicesQuery = this.apollo.watchQuery<DevicesQueryResponse, DevicesQueryVariables>({
-      notifyOnNetworkStatusChange: true, // to update the loading flag on next batch fetched
+  ): QueryRef<DevicesQueryResponse, DevicesQueryVariables> {
+    return this.apollo.watchQuery<DevicesQueryResponse, DevicesQueryVariables>({
       query: GET_DEVICES,
       fetchPolicy: 'network-only',
+      returnPartialData: true,
       variables: {
         searchOptions: {
           offset,
@@ -53,19 +50,6 @@ export class DevicesService {
           sortOptions,
           filter,
         },
-      },
-    });
-
-    return this.devicesQuery.valueChanges;
-  }
-
-  fetchMore(offset?: number, batchSize: number = 10, sortOptions?: SortOptions, filter?: string): void {
-    this.devicesQuery.refetch({
-      searchOptions: {
-        offset,
-        batchSize,
-        sortOptions,
-        filter,
       },
     });
   }
