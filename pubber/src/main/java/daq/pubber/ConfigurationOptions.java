a5b68adaef811601500b0b984679d95bd13e1b1d
Noureddine <noureddine@saidi.uk>
2022-06-21 14:36:42 +0100

    fix indent and style

--- pubber/src/main/java/daq/pubber/ConfigurationOptions.java
+++ pubber/src/main/java/daq/pubber/ConfigurationOptions.java
@@ -3,8 +3,8 @@ package daq.pubber;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Pubber configuration options which change default behavior.
@@ -15,23 +15,23 @@ public class ConfigurationOptions {
   public String extraField;
 
   /**
-   * Returns a string of enabledoptions
+   * Returns a string of enabled options and values.
    */
   public String toString() {
-  List<String> options = new ArrayList<>();
-  Field[] fields = ConfigurationOptions.class.getDeclaredFields();
+    List<String> options = new ArrayList<>();
+    Field[] fields = ConfigurationOptions.class.getDeclaredFields();
 
-  for (Field field : fields) {
-    try {
-      if (field.get(this) != null && Boolean.TRUE.equals(field.get(this))) {
-        options.add(field.getName());
-      } else if (field.get(this) != null) { 
-        options.add(field.getName() + "=" + field.get(this));
+    for (Field field : fields) {
+      try {
+        if (field.get(this) != null && Boolean.TRUE.equals(field.get(this))) {
+          options.add(field.getName());
+        } else if (field.get(this) != null) { 
+          options.add(field.getName() + "=" + field.get(this));
+        }
+      } catch (IllegalAccessException e) {
+        throw new RuntimeException(e);
       }
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
     }
-  }
   return String.join(" ", options);
 }
 
