7afa5b0fd5fceb90b50a376ca620f491ed6d59fe
John Randolph <jrand@google.com>
2022-06-23 11:46:51 -0400

    Import

--- validator/src/main/java/com/google/daq/mqtt/registrar/RegistrarTest.java
+++ validator/src/main/java/com/google/daq/mqtt/registrar/RegistrarTest.java
@@ -18,6 +18,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import org.junit.Test;
