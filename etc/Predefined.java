bea11a59351674f951e2384ef5cc8f9259e000fa
Trevor Pering <peringknife@google.com>
2022-08-04 14:03:09 -0700

    Adding shared constants file

---
+++ etc/Predefined.java
@@ -0,0 +1,10 @@
+package udmi.schema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// This class is manually curated and then copied into the gencode directory. Look for the
+// proper source and don't be fooled! These are generally useful constants.
+public static class Predefined {
+  public static final String IOT_ENDPOINT_BLOB_NAME = "_iot_config";
+}
