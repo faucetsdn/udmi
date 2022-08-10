9b6994efcf082d2c7c7b001a429293cab19fe2f8
Trevor Pering <peringknife@google.com>
2022-08-10 12:56:05 -0700

    One round of cleanup

--- etc/Category.java
+++ etc/Category.java
@@ -6,11 +6,5 @@ import static udmi.schema.Level.NOTICE;
 // This class is manually curated, auto-generated, and then copied into the gencode directory.
 // Look for the proper source and don't be fooled! Ultimately sourced from docs/specs/categories.md
 public class Category {
-
-  public static final String SYSTEM_CONFIG_RECEIVE ="system.config.receive";
-  public static final Level SYSTEM_CONFIG_RECEIVE_LEVEL = DEBUG;
-  public static final String SYSTEM_CONFIG_PARSE ="system.config.parse";
-  public static final Level SYSTEM_CONFIG_PARSE_LEVEL = DEBUG;
-  public static final String SYSTEM_CONFIG_APPLY ="system.config.apply";
-  public static final Level SYSTEM_CONFIG_APPLY_LEVEL = NOTICE;
+    @@ gencode stuff goes here
 }
