589300558f6064c782479bf20bce87b514341596
Trevor Pering <peringknife@google.com>
2022-08-10 15:02:51 -0700

    Fix gencode levels

--- etc/Category.java
+++ etc/Category.java
@@ -1,7 +1,10 @@
 package udmi.schema;
 
 import static udmi.schema.Level.DEBUG;
+import static udmi.schema.Level.INFO;
 import static udmi.schema.Level.NOTICE;
+import static udmi.schema.Level.WARNING;
+import static udmi.schema.Level.ERROR;
 
 // This class is manually curated, auto-generated, and then copied into the gencode directory.
 // Look for the proper source and don't be fooled! Ultimately sourced from docs/specs/categories.md
