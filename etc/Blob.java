3bad2f612612ff05c20c46c39deec61715d7bf23
John R <jrand@google.com>
2022-08-15 13:16:49 -0400

    Delete Blob.java

--- etc/Blob.java
+++
@@ -1,8 +0,0 @@
-package udmi.schema;
-
-// This class is manually curated and then copied into the gencode directory. Look for the
-// proper source and don't be fooled! These are general constants for blob management.
-public abstract class Blob {
-    public static final String FINAL_PHASE = "final";
-    public static final String IOT_ENDPOINT_CONFIG_BLOB = "_iot_endpoint_config";
-}
