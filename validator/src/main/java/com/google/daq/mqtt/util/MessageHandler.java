69820f55f0c84395443865ed3034b764c58fc1b1
Trevor Pering <peringknife@google.com>
2022-08-29 19:54:11 -0700

    Addressing linty

--- validator/src/main/java/com/google/daq/mqtt/util/MessageHandler.java
+++ validator/src/main/java/com/google/daq/mqtt/util/MessageHandler.java
@@ -16,9 +16,14 @@ public interface MessageHandler {
 
   /**
    * Represent a type-happy consumer into a more generic specification.
+   *
+   * @param <T> message type consumed
    */
   interface HandlerConsumer<T> extends BiConsumer<T, Envelope> { }
 
+  /**
+   * Represent a type-happy consumer into a more generic specification.
+   */
   class HandlerSpecification extends Pair<Class<?>, HandlerConsumer<?>> {
     public <T> HandlerSpecification(Class<T> valueOne, HandlerConsumer<T> valueTwo) {
       super(valueOne, valueTwo);
--- validator/src/main/java/com/google/daq/mqtt/util/Pair.java
+++ validator/src/main/java/com/google/daq/mqtt/util/Pair.java
@@ -1,12 +1,18 @@
 package com.google.daq.mqtt.util;
 
-public class Pair<TA, TB> {
+/**
+ * Simple wrapper class to handle a final pair of typed values.
+ *
+ * @param <T1> type of first value
+ * @param <T2> type of second value
+ */
+public class Pair<T1, T2> {
 
-  public Pair(TA valueOne, TB valueTwo) {
+  public Pair(T1 valueOne, T2 valueTwo) {
     this.valueOne = valueOne;
     this.valueTwo = valueTwo;
   }
 
-  public final TA valueOne;
-  public final TB valueTwo;
+  public final T1 valueOne;
+  public final T2 valueTwo;
 }
