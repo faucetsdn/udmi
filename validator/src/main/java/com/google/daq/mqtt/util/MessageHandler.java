fae9df8444dfe641eaa6d571e38b76c8b132de66
Trevor Pering <peringknife@google.com>
2022-08-29 19:08:33 -0700

    Squash commit

--- validator/src/main/java/com/google/daq/mqtt/util/MessageHandler.java
+++ validator/src/main/java/com/google/daq/mqtt/util/MessageHandler.java
@@ -15,15 +15,10 @@ public interface MessageHandler {
   void publishMessage(String deviceId, Object message);
 
   /**
-   * Consumer for a typed message with envelope.
-   *
-   * @param <T> message type consumed
+   * Represent a type-happy consumer into a more generic specification.
    */
   interface HandlerConsumer<T> extends BiConsumer<T, Envelope> { }
 
-  /**
-   * Represent a type-happy consumer into a more generic specification.
-   */
   class HandlerSpecification extends Pair<Class<?>, HandlerConsumer<?>> {
     public <T> HandlerSpecification(Class<T> valueOne, HandlerConsumer<T> valueTwo) {
       super(valueOne, valueTwo);
