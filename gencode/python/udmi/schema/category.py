ec1058d8e4d3707217be86030c0434fb688d8499
Trevor Pering <peringknife@google.com>
2022-09-02 09:40:45 -0700

    Gencode

--- gencode/python/udmi/schema/category.py
+++ gencode/python/udmi/schema/category.py
@@ -1,7 +1,7 @@
 """Generated class for category.json"""
 
 
-class Object812241E8:
+class Object35C5A1BF:
   """Generated schema class"""
 
   def __init__(self):
@@ -11,7 +11,7 @@ class Object812241E8:
   def from_dict(source):
     if not source:
       return None
-    result = Object812241E8()
+    result = Object35C5A1BF()
     return result
 
   @staticmethod
@@ -20,7 +20,7 @@ class Object812241E8:
       return None
     result = {}
     for key in source:
-      result[key] = Object812241E8.from_dict(source[key])
+      result[key] = Object35C5A1BF.from_dict(source[key])
     return result
 
   @staticmethod
