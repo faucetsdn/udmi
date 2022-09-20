e783a86be3880b2a78ff525d247e10a906a4e78a
John Randolph <jrand@google.com>
2022-09-20 17:45:52 -0400

    x

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/BlobsetSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/BlobsetSequences.java
@@ -1,6 +1,7 @@
 package com.google.daq.mqtt.sequencer.sequences;
 
 import static udmi.schema.Category.BLOBSET_BLOB_APPLY;
+import static udmi.schema.Category.SYSTEM_CONFIG_APPLY;
 
 import com.google.daq.mqtt.sequencer.SequenceBase;
 import java.util.Base64;
@@ -28,7 +29,7 @@ public class BlobsetSequences extends SequenceBase {
 
   // TODO: Build this from the site model config.
   private static final String ENDPOINT_CONFIG_CLIENT_ID =
-      "projects/bos-johnrandolph-dev/locations/us-central1/registries/ZZ-TRI-FECTA/devices/ZZ-TRI-FECTA";
+      "projects/bos-johnrandolph-dev/locations/us-central1/registries/ZZ-TRI-FECTA/devices/AHU-1";
   private static final String ENDPOINT_CONFIG_HOSTNAME = "mqtt.googleapis.com";
 
   private String endpointConfigPayloadBase64(String hostname) {
@@ -66,11 +67,16 @@ public class BlobsetSequences extends SequenceBase {
     deviceConfig.blobset.blobs = new HashMap<>();
     deviceConfig.blobset.blobs.put(SystemBlobsets.IOT_ENDPOINT_CONFIG.value(), config);
 
-    untilTrue("blobset entry config status is error", () -> {
-      Entry stateStatus = deviceState.blobset.blobs.get(
-          SystemBlobsets.IOT_ENDPOINT_CONFIG.value()).status;
-      return stateStatus.category.equals(BLOBSET_BLOB_APPLY)
-          && stateStatus.level == Level.ERROR.value();
+    untilTrue("blobset entry config status is success", () -> {
+      BlobPhase phase = deviceState.blobset.blobs.get(
+          SystemBlobsets.IOT_ENDPOINT_CONFIG.value()).phase;
+      Entry stateStatus = deviceState.system.status;
+      if (phase==null) info("A");
+      if (stateStatus==null) info("B");
+      return phase != null
+          && phase.equals(BlobPhase.FINAL)
+          && stateStatus.category.equals(SYSTEM_CONFIG_APPLY)
+          && stateStatus.level == Level.NOTICE.value();
     });
   }
 }
