88f6ecf352286af59e6f2b24ea139cb182a42815
Marc Spratt <marc.spratt@kodaro.com>
2022-09-26 16:13:39 -0400

    #1462-now redirect using guard

--- udmif/web/src/app/login/login.guard.ts
+++ udmif/web/src/app/login/login.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { filter, map, Observable } from 'rxjs';
+import { map, Observable, shareReplay } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable({
@@ -9,16 +9,19 @@ import { AuthService } from '../auth/auth.service';
 export class LoginGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+  canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<boolean | UrlTree> {
     return this.authService.isLoggedIn$.pipe(
-      filter((isLoggedIn) => isLoggedIn !== null), // loading, so short circuit
       map((isLoggedIn) => {
         if (isLoggedIn) {
-          return this.router.parseUrl('/devices');
+          // The subscription gets ignored after initial route guard check, so
+          // we need to manually navigate instead of return a UrlTree.
+          this.router.navigateByUrl('/devices');
+          return false;
         } else {
           return true;
         }
-      })
+      }),
+      shareReplay(1) // share to trigger map to run again, thus triggering the navigateByUrl
     );
   }
 }
