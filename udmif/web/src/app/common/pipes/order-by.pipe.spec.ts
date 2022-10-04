4a122c188d1acb8e61d68ad5ee52025798ba4723
Marc Spratt <marc.spratt@kodaro.com>
2022-10-04 11:23:28 -0400

    #1417-fixed tests

--- udmif/web/src/app/common/pipes/order-by.pipe.spec.ts
+++ udmif/web/src/app/common/pipes/order-by.pipe.spec.ts
@@ -1,18 +1,44 @@
-import { CapitalizePipe } from './capitalize.pipe';
+import { OrderByPipe } from './order-by.pipe';
 
-describe('CapitalizePipe', () => {
-  const pipe = new CapitalizePipe();
+describe('OrderByPipe', () => {
+  const pipe = new OrderByPipe();
 
   const testCases = [
-    { value: null, expected: '' },
-    { value: undefined, expected: '' },
-    { value: '', expected: '' },
-    { value: 'errorCount', expected: 'Error Count' },
+    { expected: [] },
+    { value: null, sortBy: null, order: null, expected: [] },
+    {
+      value: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+      sortBy: 'name',
+      expected: [{ name: 'device-1' }, { name: 'device-2' }, { name: 'device-3' }],
+    },
+    {
+      value: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+      sortBy: 'name',
+      order: 'asc',
+      expected: [{ name: 'device-1' }, { name: 'device-2' }, { name: 'device-3' }],
+    },
+    {
+      value: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+      sortBy: 'name',
+      order: 'desc',
+      expected: [{ name: 'device-3' }, { name: 'device-2' }, { name: 'device-1' }],
+    },
+    {
+      value: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+      sortBy: 'bogus',
+      expected: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+    },
+    {
+      value: [{ name: 'device-1' }, { name: 'device-3' }, { name: 'device-2' }],
+      sortBy: 'name',
+      order: 'bogus',
+      expected: [{ name: 'device-1' }, { name: 'device-2' }, { name: 'device-3' }],
+    },
   ];
 
   testCases.forEach((test) => {
     it(`should transform ${test.value} to ${test.expected}`, () => {
-      expect(pipe.transform(test.value)).toEqual(test.expected);
+      expect(pipe.transform(test.value, test.sortBy, test.order as 'asc' | 'desc')).toEqual(test.expected);
     });
   });
 });
