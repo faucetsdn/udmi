50444ea8e0fdf44fbc20a5add877380ecfc58937
Trevor <grafnu@users.noreply.github.com>
2022-11-21 14:13:46 -0800

    Enable metadata schema validation (#504)

--- validator/src/main/java/com/google/daq/mqtt/registrar/LocalDevice.java
+++ validator/src/main/java/com/google/daq/mqtt/registrar/LocalDevice.java
@@ -301,23 +301,28 @@ class LocalDevice {
       return null;
     }
 
+    JsonNode mergedMetadata = getMergedMetadata(instance);
+
     try {
-      ProcessingReport report = schemas.get(METADATA_JSON).validate(instance);
+      ProcessingReport report = schemas.get(METADATA_JSON).validate(mergedMetadata);
       if (validate) {
         parseMetadataValidateProcessingReport(report);
       }
     } catch (ProcessingException | ValidationException e) {
       exceptionMap.put(EXCEPTION_VALIDATING, e);
     }
+    return JsonUtil.convertTo(Metadata.class, mergedMetadata);
+  }
 
+  JsonNode getMergedMetadata(JsonNode instance) {
     try {
       String intermediary = JsonUtil.stringify(instance);
       if (siteMetadata == null) {
-        return JsonUtil.fromString(Metadata.class, intermediary);
+        return instance;
       } else {
         Map<String, Object> mergedMetadata = GeneralUtils.deepCopy(siteMetadata);
         GeneralUtils.mergeObject(mergedMetadata, asMap(intermediary));
-        return JsonUtil.convertTo(Metadata.class, mergedMetadata);
+        return JsonUtil.convertTo(JsonNode.class, mergedMetadata);
       }
     } catch (Exception e) {
       exceptionMap.put(EXCEPTION_READING, e);
