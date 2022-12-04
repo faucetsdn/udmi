d9c558533c8678ab3c9364f1fa6292f116d8255a
Trevor Pering <peringknife@google.com>
2022-12-03 21:26:49 -0800

    Cleanup

--- validator/src/main/java/com/google/bos/iot/core/proxy/MockPublisher.java
+++ validator/src/main/java/com/google/bos/iot/core/proxy/MockPublisher.java
@@ -16,6 +16,11 @@ public class MockPublisher implements MessagePublisher {
   private final BlockingQueue<Validator.MessageBundle> messages = new LinkedBlockingQueue<>();
   private boolean active = true;
 
+  /**
+   * Create a mock publisher that can optionally fail fast on any operation.
+   *
+   * @param failFast true to fail immediately
+   */
   public MockPublisher(boolean failFast) {
     active = !failFast;
   }
