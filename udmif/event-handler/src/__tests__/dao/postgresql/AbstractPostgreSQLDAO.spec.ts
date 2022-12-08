2af363b00ea37d61dc04e14e61770186a304ee3d
slevert <stephane.levert@buildingsiot.com>
2022-12-08 12:55:47 -0500

    cleaning up unit tests

--- udmif/event-handler/src/__tests__/dao/postgresql/AbstractPostgreSQLDAO.spec.ts
+++ udmif/event-handler/src/__tests__/dao/postgresql/AbstractPostgreSQLDAO.spec.ts
@@ -21,6 +21,7 @@ describe('AbstractPostgreSQLDAO.insert', () => {
     await db.schema.createTable('any', function (table) {
       table.string('id', 255);
       table.string('name', 255);
+      table.string('value', 255);
       table.unique(['id']);
     });
     testClass = new TestClass(db);
@@ -35,15 +36,14 @@ describe('AbstractPostgreSQLDAO.insert', () => {
     const result = await testClass.get({ id: 'some-id' });
 
     // assert
-    expect(result).toEqual(object);
+    expect(result).toEqual({ ...object, value: null });
   });
 
   test('we can get an upserted value', async () => {
     // arrange
     const object1 = { id: 'some-id', name: 'name1' };
+    const object2 = { id: 'some-id', name: 'name1', value: 'some-value' };
     await testClass.insert(object1);
-
-    const object2 = { id: 'some-id', name: 'name1' };
 
     // act
     await testClass.upsert(object2, ['id']);
