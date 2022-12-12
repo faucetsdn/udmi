03e46b48ee43de9f2cc2032dad4933e59e7925f5
noursaidi <noureddine@saidi.uk>
2022-12-12 12:56:32 +0000

    misc

--- misc/gcloud_messages_telemetry/functions/index.js
+++ misc/gcloud_messages_telemetry/functions/index.js
@@ -5,11 +5,11 @@ const PROJECT_ID = process.env.PROJECT_ID
 const DATASET_ID = process.env.DATASET_ID
 const MESSAGES_TABLE = process.env.MESSAGES_TABLE
 const TELEMETRY_TABLE = process.env.TELEMETRY_TABLE
+const STATE_TABLE = process.env.STATE_TABLE
 
 const STATE = 1
 const EVENT_POINTSET = 2
 const EVENT_SYSTEM = 3
-const EVENT_DISCOVERY = 4
 const EVENT_OTHER = 9
 
 exports.processMessage = async (event, context) => {
@@ -19,17 +19,17 @@ exports.processMessage = async (event, context) => {
   } else if (!event.attributes.hasOwnProperty('subType')) {
     if (event.attributes.subFolder == 'pointset'){
       var messageType = EVENT_POINTSET;
-    } else if (event.attributes.subFolder == 'pointset'){
+    } else if (event.attributes.subFolder == 'system'){
       var messageType = EVENT_SYSTEM;
     } else {
       var messageType = EVENT_OTHER
     }
   } else {
+    // Not a message from IoT Core
     return;
   }
   const pubsubMessage = event.data;
   const objStr = Buffer.from(pubsubMessage, 'base64').toString('utf8');
-  const msgObj = JSON.parse(objStr);
 
   // This is a Pub/Sub message ID -  messages copied in the stack 
   // will have different message IDs
@@ -50,7 +50,7 @@ exports.processMessage = async (event, context) => {
     device_num_id: parseInt(event.attributes.deviceNumId),
     device_id: deviceId,
     gateway_id: gatewayId,
-    registry_id: deviceRegistryId,
+    device_registry_id: deviceRegistryId,
     message_id: messageId,
     payload_size_bytes: payloadSize,
     message_type: messageType
@@ -58,22 +58,29 @@ exports.processMessage = async (event, context) => {
 
   promises.push(bigquery.dataset(DATASET_ID).table(MESSAGES_TABLE).insert([messageRow]));
   
-  // Insert Telemetry
-  if ('points' in msgObj){
+  try {
+    var msg = JSON.parse(objStr);
+  } catch {
+    console.log("${deviceRegistryId}/${deviceId} Invalid JSON");
+    return await Promise.all(promises);
+  }
+
+  // Insert telemetry if pointset event
+  // TODO break out into processTelemetry
+  if (messageType == EVENT_POINTSET && 'points' in msg){
     var rows = []
-    const payloadTimestamp = BigQuery.timestamp(msgObj.timestamp);
-    Object.keys(msgObj.points).forEach(function(key) {
+    Object.keys(msg.points).forEach(function(key) {
       let row = {
         device_id: deviceId,
         device_num_id: parseInt(event.attributes.deviceNumId),
         message_id: messageId,
         gateway_id: gatewayId,
-        registry_id: deviceRegistryId,
+        device_registry_id: deviceRegistryId,
         publish_timestamp: publishTimestamp,
-        timestamp: payloadTimestamp,
+        timestamp: BigQuery.timestamp(msg.timestamp),
         point_name: key,
-        present_value: (isNaN(parseFloat(msgObj.points[key].present_value)) ? null : parseFloat(msgObj.points[key].present_value) ),
-        present_value_string: String(msgObj.points[key].present_value)
+        present_value: (isNaN(parseFloat(msg.points[key].present_value)) ? null : parseFloat(msg.points[key].present_value) ),
+        present_value_string: String(msg.points[key].present_value)
       };
         rows.push(row)    
     });
@@ -81,8 +88,71 @@ exports.processMessage = async (event, context) => {
      if (rows.length > 0){
       promises.push(bigquery.dataset(DATASET_ID).table(TELEMETRY_TABLE).insert(rows))
      }
-   
+
+  } else if (messageType == STATE) {
+    //TODO break out to processState
+
+    if ("system" in msg === false) {
+      return await Promise.all(promises);
+    }
+
+    // TODO break out to upgradeState 
+    const softwareEntries = []
+
+    // Upgrade software as applicable
+    if("firmware" in msg.system && "version" in msg.system.firmware) {
+      // Legacy (<1.3.14)
+      softwareEntries.push({id: "firmware", version:msg.system.firmware.version});
+    } else {
+      // Modern (>1.3.14)
+      for (const [key, value] of Object.entries(msg.system.software)) {
+        if (isString(key) && isString(value)){
+          softwareEntries.push({id: key, version:value})
+        }
+      }
+    }
+    
+    // Upgrade Hardware
+    if("make_model" in msg.system && isString(msg.system.make_model)){
+      // Legacy (<1.3.14)
+      make = "unknown";
+      model = msg.system.make_model;
+      rev = null;
+      sku = null;
+    } else {
+      make = ("make" in msg.system.hardware && isString(msg.system.hardware.make)) 
+        && msg.system.hardware.make || null;
+      model = ("model" in msg.system.hardware && isString(msg.system.hardware.model)) 
+        && msg.system.hardware.model || null;
+      rev = ("rev" in msg.system.hardware && isString(msg.system.hardware.rev)) 
+        && msg.system.hardware.rev || null;
+      sku = ("sku" in msg.system.hardware && isString(msg.system.hardware.sku)) 
+        && msg.system.hardware.sku || null;
+    }
+
+    // Data for devices table
+    stateRow = {
+      timestamp: BigQuery.timestamp(msg.timestamp),
+      publish_timestamp: publishTimestamp,
+      device_id: event.attributes.deviceId,
+      device_num_id: parseInt(event.attributes.deviceNumId),
+      gateway_id: gatewayId,
+      device_registry_id: event.attributes.deviceRegistryId,
+      message_id: messageId,
+      make: make,
+      model: model,
+      serial_no: ("serial_no" in msg.system) && msg.system.serial_no || null,
+      rev: rev,
+      sku: sku,
+      software: softwareEntries
+    }
+
+    promises.push(bigquery.dataset(DATASET_ID).table(STATE_TABLE).insert([stateRow]));
   }
  
   return await Promise.all(promises);
 };
+
+function isString(variable){
+  return (typeof variable == "string");
+}
