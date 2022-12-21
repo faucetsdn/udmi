ef707b1aabb543a6c96dbeaffe2c6cd76d7c947a
Noureddine <noureddine@saidi.uk>
2022-12-21 15:34:55 +0000

    fix order

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/ConfigSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/ConfigSequences.java
@@ -21,7 +21,6 @@ import com.google.daq.mqtt.util.SamplingRange;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
