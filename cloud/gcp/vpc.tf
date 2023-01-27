ed3f9010d46d8ee510552c05194eacc716c58094
Narmadha Koti <narmadha.koti@buildingsiot.com>
2023-01-27 11:03:56 -0500

    corrected postgres network and removed mongoDb from terraform

--- cloud/gcp/vpc.tf
+++ cloud/gcp/vpc.tf
@@ -18,6 +18,6 @@ resource "google_compute_subnetwork" "subnet" {
 resource "google_vpc_access_connector" "connector" {
   name          = "udmi-cf-sql-vpc"
   ip_cidr_range = "10.9.0.0/28"
-  network       = "private-network"
+  network       = "default"
   max_throughput = 300
 } 
