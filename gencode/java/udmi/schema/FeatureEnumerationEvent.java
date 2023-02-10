8e379fc1183200360e749266ff415e57d659da2d
Trevor Pering <peringknife@google.com>
2023-02-09 19:50:54 -0800

    Updating feature enumeration

--- gencode/java/udmi/schema/FeatureEnumerationEvent.java
+++ gencode/java/udmi/schema/FeatureEnumerationEvent.java
@@ -1,11 +1,15 @@
 
 package udmi.schema;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 
 /**
@@ -16,23 +20,23 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "networks"
+    "stage"
 })
 @Generated("jsonschema2pojo")
 public class FeatureEnumerationEvent {
 
     /**
-     * Collection of networks that this feature applies to
+     * Feature implementation stage
      * 
      */
-    @JsonProperty("networks")
-    @JsonPropertyDescription("Collection of networks that this feature applies to")
-    public Networks networks;
+    @JsonProperty("stage")
+    @JsonPropertyDescription("Feature implementation stage")
+    public FeatureEnumerationEvent.Stage stage;
 
     @Override
     public int hashCode() {
         int result = 1;
-        result = ((result* 31)+((this.networks == null)? 0 :this.networks.hashCode()));
+        result = ((result* 31)+((this.stage == null)? 0 :this.stage.hashCode()));
         return result;
     }
 
@@ -45,7 +49,54 @@ public class FeatureEnumerationEvent {
             return false;
         }
         FeatureEnumerationEvent rhs = ((FeatureEnumerationEvent) other);
-        return ((this.networks == rhs.networks)||((this.networks!= null)&&this.networks.equals(rhs.networks)));
+        return ((this.stage == rhs.stage)||((this.stage!= null)&&this.stage.equals(rhs.stage)));
+    }
+
+
+    /**
+     * Feature implementation stage
+     * 
+     */
+    @Generated("jsonschema2pojo")
+    public enum Stage {
+
+        MISSING("missing"),
+        ALPHA("alpha"),
+        BETA("beta"),
+        STABLE("stable");
+        private final String value;
+        private final static Map<String, FeatureEnumerationEvent.Stage> CONSTANTS = new HashMap<String, FeatureEnumerationEvent.Stage>();
+
+        static {
+            for (FeatureEnumerationEvent.Stage c: values()) {
+                CONSTANTS.put(c.value, c);
+            }
+        }
+
+        Stage(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return this.value;
+        }
+
+        @JsonValue
+        public String value() {
+            return this.value;
+        }
+
+        @JsonCreator
+        public static FeatureEnumerationEvent.Stage fromValue(String value) {
+            FeatureEnumerationEvent.Stage constant = CONSTANTS.get(value);
+            if (constant == null) {
+                throw new IllegalArgumentException(value);
+            } else {
+                return constant;
+            }
+        }
+
     }
 
 }
