ac771fc6a42ffc2ce4a44359b3df091c08adf045
Trevor <grafnu@users.noreply.github.com>
2023-02-14 14:39:22 -0800

    Add options and itemized tests for pubber failure modes (#588)

--- gencode/python/udmi/schema/options_pubber.py
+++ gencode/python/udmi/schema/options_pubber.py
@@ -8,6 +8,8 @@ class PubberOptions:
     self.fixedSampleRate = None
     self.noHardware = None
     self.noConfigAck = None
+    self.noPersist = None
+    self.barfConfig = None
     self.messageTrace = None
     self.extraPoint = None
     self.missingPoint = None
@@ -25,6 +27,8 @@ class PubberOptions:
     result.fixedSampleRate = source.get('fixedSampleRate')
     result.noHardware = source.get('noHardware')
     result.noConfigAck = source.get('noConfigAck')
+    result.noPersist = source.get('noPersist')
+    result.barfConfig = source.get('barfConfig')
     result.messageTrace = source.get('messageTrace')
     result.extraPoint = source.get('extraPoint')
     result.missingPoint = source.get('missingPoint')
@@ -59,6 +63,10 @@ class PubberOptions:
       result['noHardware'] = self.noHardware # 5
     if self.noConfigAck:
       result['noConfigAck'] = self.noConfigAck # 5
+    if self.noPersist:
+      result['noPersist'] = self.noPersist # 5
+    if self.barfConfig:
+      result['barfConfig'] = self.barfConfig # 5
     if self.messageTrace:
       result['messageTrace'] = self.messageTrace # 5
     if self.extraPoint:
