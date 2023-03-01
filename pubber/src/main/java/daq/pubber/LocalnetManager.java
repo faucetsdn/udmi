8ececb602693f4a5ec85140454176bd19cd7f6e0
Trevor Pering <peringknife@google.com>
2023-02-28 22:30:54 -0800

    Add diagnostic interface output

--- pubber/src/main/java/daq/pubber/LocalnetManager.java
+++ pubber/src/main/java/daq/pubber/LocalnetManager.java
@@ -38,17 +38,16 @@ public class LocalnetManager {
 
   /**
    * Parse the output of ip route/addr and turn it into a family addr map.
+   * <p>
+   * Start with default route with lowest metric, and then parse the addresses for that interface.
    *
-   * Start with default route with lowest metric.
-   *
+   * <pre>
    * peringknife@peringknife-glaptop4:~/udmi$ ip route
    * default via 192.168.8.1 dev enp0s31f6 proto dhcp src 192.168.8.3 metric 100
    * default via 10.0.0.1 dev wlp0s20f3 proto dhcp src 10.0.0.142 metric 600
    * 10.0.0.0/24 dev wlp0s20f3 proto kernel scope link src 10.0.0.142 metric 600
    * 192.168.8.0/24 dev enp0s31f6 proto kernel scope link src 192.168.8.3 metric 100
    * 192.168.9.0/24 dev docker0 proto kernel scope link src 192.168.9.1 linkdown
-   *
-   * And then parse the addresses for that interface.
    *
    * peringknife@peringknife-glaptop4:~/udmi$ ip addr show dev enp0s31f6
    * 2: enp0s31f6: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc fq_codel state UP group default qlen 1000
@@ -57,11 +56,14 @@ public class LocalnetManager {
    *        valid_lft 83596sec preferred_lft 83596sec
    *     inet6 fe80::11a9:496f:6596:b455/64 scope link noprefixroute
    *        valid_lft forever preferred_lft forever
+   * </pre>
    */
   private HashMap<String, FamilyLocalnetState> enumerateInterfaceAddresses() {
     String defaultInterface = getDefaultInterface();
     parent.info("Using addresses from default interface " + defaultInterface);
     Map<String, String> interfaceAddresses = getInterfaceAddresses(defaultInterface);
+    interfaceAddresses.forEach(
+        (key, value) -> parent.info("Family " + key + " address is " + value));
     return null;
   }
 
