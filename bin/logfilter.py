4e193a00e744ec3ba5bfc708e0b31b8ce4f6190f
John Randolph <jrand@google.com>
2023-03-03 18:12:19 -0500

    x

--- bin/logfilter.py
+++ bin/logfilter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Filter log output."""
 import re
 import sys
 import json
@@ -7,23 +8,23 @@ import getopt
 LINE_BREAK = '=' * 75
 
 RE_SEQUENCER_LINE = re.compile(
-  '^(?P<date>\d{4}-\d{2}-\d{2})T'
-  '(?P<time>\d\d:\d\d:\d\d)Z\s'
-  '(?P<loglevel>[^\s]+)\s'
-  '(?P<process>sequencer)\s+'
-  '(?P<tail>.*)')
+  r'^(?P<date>\d{4}-\d{2}-\d{2})T'
+  r'(?P<time>\d\d:\d\d:\d\d)Z\s'
+  r'(?P<loglevel>[^\s]+)\s'
+  r'(?P<process>sequencer)\s+'
+  r'(?P<tail>.*)')
 
 RE_PUBBER_LINE = re.compile(
-  '^INFO daq.pubber.(?P<class>\w+) - '
-  '(?P<date>\d{4}-\d{2}-\d{2})T'
-  '(?P<time>\d\d:\d\d:\d\d)Z\s'
-  '(?P<tail>.*)')
+  r'^INFO daq.pubber.(?P<class>\w+) - '
+  r'(?P<date>\d{4}-\d{2}-\d{2})T'
+  r'(?P<time>\d\d:\d\d:\d\d)Z\s'
+  r'(?P<tail>.*)')
 
 RE_SEQUENCER_TAIL_JSON_BEGIN = re.compile(
-  '^Updated (?P<type>config|state) \#(?P<num>\d+):$')
+  r'^Updated (?P<type>config|state) \#(?P<num>\d+):$')
 
 RE_SEQUENCER_TAIL_TEST_BEGIN_END = re.compile(
-  '^(?P<mode>starting|ending) test (?P<name>[a-z_]+)')
+  r'^(?P<mode>starting|ending) test (?P<name>[a-z_]+)')
 
 RE_PUBBER_TAIL_JSON_BEGIN = re.compile(
   r'^(?P<type>Config|State) update(|\s+\(\w+\)):$')
@@ -49,17 +50,16 @@ def debug_print(*args):
   fd_print(sys.stderr, *args)
 
 def json_diff_print(json_obj1, json_obj2):
-  from _pytest.assertion.util import _compare_eq_any
+  from _pytest.assertion.util import _compare_eq_any  # pylint: disable=import-outside-toplevel
   output_print('======JSON WITH DIFF')
-  output_print(json.dumps(json_obj2, indent=2)) 
+  output_print(json.dumps(json_obj2, indent=2))
   output_print('======DIFF BEGIN')
   output_print('\n'.join(_compare_eq_any(json_obj1, json_obj2, verbose=3)))
   output_print('======DIFF END')
 
 def json_stack_print(json_type, json_num):
-  global json_stack
-
-  if (json_num-1) in json_stack[json_type]:
+  global json_stack  # pylint: disable=global-variable-not-assigned
+  if json_num-1 in json_stack[json_type]:
     json_diff_print(
       json_stack[json_type][json_num-1],
       json_stack[json_type][json_num])
@@ -75,17 +75,18 @@ def parse_log_file(filename, github_action=False):
   global json_stack
   json_stack = {'config': {}, 'state': {}}
 
-  f = open(filename, 'r')
+  f = open(filename, 'r', encoding='ascii')
   for l in f:
     l = l.strip('\n')
     if github_action:
       l = l[29:]
-    debug_print('[in_json=%s (%s,%s)] %s', in_json, in_json_type, in_json_num, l)
-    m_seq = RE_SEQUENCER_LINE.match(l) 
+    debug_print(
+      '[in_json=%s (%s,%s)] %s', in_json, in_json_type, in_json_num, l)
+    m_seq = RE_SEQUENCER_LINE.match(l)
     m_pub = RE_PUBBER_LINE.match(l)
 
     if m_pub:
-      assert(in_json==False)
+      assert in_json is False
       debug_print(m_pub.groupdict())
       m2 = RE_PUBBER_TAIL_JSON_BEGIN.match(m_pub['tail'])
       if m2:
@@ -97,9 +98,9 @@ def parse_log_file(filename, github_action=False):
       output_print(l)
 
     if m_seq:
-      assert(in_json==False)
+      assert in_json is False
       debug_print(m_seq.groupdict())
-      m2 = RE_SEQUENCER_TAIL_JSON_BEGIN.match(m_seq['tail']) 
+      m2 = RE_SEQUENCER_TAIL_JSON_BEGIN.match(m_seq['tail'])
       if m2:
         debug_print(m2.groupdict())
         in_json_type = m2.groupdict()['type']
@@ -116,7 +117,7 @@ def parse_log_file(filename, github_action=False):
       else:
         output_print(l)
 
-    if not m_pub and not m_seq and in_json: 
+    if not m_pub and not m_seq and in_json:
       if RE_FILTER_INVALID_JSON_LINE.match(l):
         debug_print('Skipping invalid line: %s', l)
       else:
@@ -127,7 +128,7 @@ def parse_log_file(filename, github_action=False):
           in_json = False
         else:
           json_lines.append(l)
-   
+
     if not in_json and json_lines:
       debug_print(json_lines)
       json_obj = json.loads('\n'.join(json_lines))
@@ -143,7 +144,7 @@ def parse_log_file(filename, github_action=False):
 def main(args):
   options, args = getopt.getopt(args[1:], 'g')
   github_action = False
-  for opt, optarg in options:
+  for opt, unused_optarg in options:
     if opt == '-g':
       github_action = True
   parse_log_file(args[0], github_action=github_action)
