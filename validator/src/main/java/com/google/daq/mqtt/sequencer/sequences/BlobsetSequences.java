ecb8cef30fa45ed540560e05e8884f2f3e70f3d3
Trevor Pering <peringknife@google.com>
2023-03-09 12:14:03 -0800

    Cleanup some testing flow

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/BlobsetSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/BlobsetSequences.java
@@ -1,5 +1,6 @@
 package com.google.daq.mqtt.sequencer.sequences;
 
+import static com.google.daq.mqtt.util.TimePeriodConstants.TWO_MINUTES_MS;
 import static com.google.udmi.util.GeneralUtils.encodeBase64;
 import static com.google.udmi.util.GeneralUtils.sha256;
 import static com.google.udmi.util.JsonUtil.stringify;
@@ -191,6 +192,7 @@ public class BlobsetSequences extends SequenceBase {
   }
 
   @Test
+  @Test(timeout = TWO_MINUTES_MS)
   @Feature(stage = ALPHA, bucket = ENDPOINT)
   public void endpoint_failure_and_restart() {
     setDeviceConfigEndpointBlob(BOGUS_ENDPOINT_HOSTNAME, registryId, false);
