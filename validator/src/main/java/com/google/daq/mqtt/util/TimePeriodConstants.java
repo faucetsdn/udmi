6607aba9e0e780c9452fbe1f3d043100fb9e841d
Trevor Pering <peringknife@google.com>
2023-03-18 10:03:57 -0700

    Squash commit for cherry-pick into 1.4.1

--- validator/src/main/java/com/google/daq/mqtt/util/TimePeriodConstants.java
+++ validator/src/main/java/com/google/daq/mqtt/util/TimePeriodConstants.java
@@ -5,8 +5,12 @@ package com.google.daq.mqtt.util;
  */
 public class TimePeriodConstants {
   public static final int NINETY_SECONDS_MS = 2 * 60 * 1000;
+  public static final int ONE_MINUTES_MS = 2 * 60 * 1000;
   public static final int TWO_MINUTES_MS = 2 * 60 * 1000;
   public static final int THREE_MINUTES_MS = 3 * 60 * 1000;
   public static final int FOUR_MINUTES_MS = 4 * 60 * 1000;
 
+  public static int minutesMs(int x) {
+    return x * 60 * 1000;
+  }
 }
