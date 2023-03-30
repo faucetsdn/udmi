2abb26241ee1849e0f78ff1e367f3ae916d38de5
Trevor Pering <peringknife@google.com>
2023-03-29 18:57:14 -0700

    Adding quantized barrier

--- validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
@@ -106,6 +106,7 @@ public class SequenceBase {
   private static final int FUNCTIONS_VERSION_BETA = Validator.REQUIRED_FUNCTION_VER;
   private static final int FUNCTIONS_VERSION_ALPHA = 6; // Version required for alpha execution.
   private static final String START_END_MARKER = "################################";
+  private static final long CONFIG_BARRIER_MS = 1000;
 
   static {
     // Sanity check to make sure ALPHA version is increased if forced by increased BETA.
@@ -737,6 +738,8 @@ public class SequenceBase {
 
   protected void updateConfig(String reason) {
     assertConfigIsNotPending();
+    // Add a forced sleep to make sure second-quantized timestamps are unique.
+    safeSleep(CONFIG_BARRIER_MS);
     updateConfig(SubFolder.SYSTEM, augmentConfig(deviceConfig.system));
     updateConfig(SubFolder.POINTSET, deviceConfig.pointset);
     updateConfig(SubFolder.GATEWAY, deviceConfig.gateway);
