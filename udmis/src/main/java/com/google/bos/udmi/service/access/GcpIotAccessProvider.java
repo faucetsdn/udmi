8a45bb297aab66f2c05bcaf3ddf8d1fc5bbb06fe
Trevor Pering <peringknife@google.com>
2023-04-28 21:41:06 -0700

    Fetch GCP registry regions

--- udmis/src/main/java/com/google/bos/udmi/service/access/GcpIotAccessProvider.java
+++ udmis/src/main/java/com/google/bos/udmi/service/access/GcpIotAccessProvider.java
@@ -13,15 +13,23 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.cloudiot.v1.CloudIot;
 import com.google.api.services.cloudiot.v1.CloudIotScopes;
 import com.google.api.services.cloudiot.v1.model.DeviceConfig;
+import com.google.api.services.cloudiot.v1.model.DeviceRegistry;
+import com.google.api.services.cloudiot.v1.model.ListDeviceRegistriesResponse;
 import com.google.api.services.cloudiot.v1.model.ModifyCloudToDeviceConfigRequest;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.bos.udmi.service.core.UdmisComponent;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 import udmi.schema.Credential.Key_format;
 import udmi.schema.Envelope.SubFolder;
 import udmi.schema.IotAccess;
@@ -29,12 +37,15 @@ import udmi.schema.IotAccess;
 /**
  * IoT access provider for (deprecated) GCP IoT Core.
  */
-public class GcpIotAccessProvider implements IotAccessProvider {
+public class GcpIotAccessProvider extends UdmisComponent implements IotAccessProvider {
 
+  private static final String EMPTY_JSON = "{}";
   private static final String PROJECT_PATH_FORMAT = "projects/%s";
-  private static final String REGISTRY_PATH_FORMAT = "%s/locations/%s/registries/%s";
+  private static final String LOCATIONS_PATH_FORMAT = "%s/locations/%s";
+  private static final String REGISTRY_PATH_FORMAT = "%s/registries/%s";
   private static final String DEVICE_PATH_FORMAT = "%s/devices/%s";
-  private static final String DEFAULT_CLOUD_REGION = "us-central1";
+  private static final Set<String> CLOUD_REGIONS =
+      ImmutableSet.of("us-central1", "europe-west1", "asia-east1");
   private static final String APPLICATION_NAME = "com.google.iot.bos";
   private static final String RSA_KEY_FORMAT = "RSA_PEM";
   private static final String RSA_CERT_FORMAT = "RSA_X509_PEM";
@@ -46,18 +57,19 @@ public class GcpIotAccessProvider implements IotAccessProvider {
           Key_format.RS_256_X_509, RSA_CERT_FORMAT,
           Key_format.ES_256, ES_KEY_FORMAT,
           Key_format.ES_256_X_509, ES_CERT_FILE);
-  public static final String EMPTY_JSON = "{}";
-
   private final String projectId;
-  private final Map<String, String> registryCloudRegions = new ConcurrentHashMap<>();
+  private final Map<String, String> registryCloudRegions;
+  private final CloudIot cloudIotService;
   private CloudIot.Projects.Locations.Registries registries;
 
   public GcpIotAccessProvider(IotAccess iotAccess) {
     projectId = requireNonNull(iotAccess.project_id, "gcp project id not specified");
+    cloudIotService = createCloudIotService();
+    registryCloudRegions = fetchRegistryCloudRegions();
   }
 
-  @Override
-  public void activate() {
+  @NotNull
+  private CloudIot createCloudIotService() {
     try {
       GoogleCredentials credential = GoogleCredentials.getApplicationDefault()
           .createScoped(CloudIotScopes.all());
@@ -65,25 +77,10 @@ public class GcpIotAccessProvider implements IotAccessProvider {
       HttpRequestInitializer init = new HttpCredentialsAdapter(credential);
       CloudIot cloudIotService = new CloudIot.Builder(GoogleNetHttpTransport.newTrustedTransport(),
           jsonFactory, init).setApplicationName(APPLICATION_NAME).build();
-      registries = cloudIotService.projects().locations().registries();
+      return cloudIotService;
     } catch (Exception e) {
-      throw new RuntimeException("While activating", e);
+      throw new RuntimeException("While creating GCP IoT Core service", e);
     }
-  }
-
-  @Override
-  public void shutdown() {
-    registries = null;
-  }
-
-  @Override
-  public void modifyConfig(String registryId, String deviceId, SubFolder subFolder,
-      String contents) {
-    // TODO: Need to implement checking of config version for concurrent operations.
-    String configString = ofNullable(fetchConfig(registryId, deviceId)).orElse(EMPTY_JSON);
-    Map<String, Object> configMap = toMap(configString);
-    configMap.put(subFolder.toString(), contents);
-    updateConfig(registryId, deviceId, stringify(configMap));
   }
 
   private String fetchConfig(String registryId, String deviceId) {
@@ -99,6 +96,71 @@ public class GcpIotAccessProvider implements IotAccessProvider {
     }
   }
 
+  private Map<String, String> fetchRegistryCloudRegions() {
+    Map<String, String> regionMap = CLOUD_REGIONS.stream().map(this::getRegistriesForRegion)
+        .flatMap(map -> map.entrySet().stream())
+        .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+    debug(format("Fetched %s registry regions", regionMap.size()));
+    return regionMap;
+  }
+
+  private String getDevicePath(String registryId, String deviceId) {
+    return format(DEVICE_PATH_FORMAT, getRegistryPath(registryId), deviceId);
+  }
+
+  private String getLocationPath(String cloudRegion) {
+    return format(LOCATIONS_PATH_FORMAT, getProjectPath(), cloudRegion);
+  }
+
+  private String getProjectPath() {
+    return format(PROJECT_PATH_FORMAT, projectId);
+  }
+
+  @NotNull
+  private Map<String, String> getRegistriesForRegion(String region) {
+    try {
+      debug("Fetching registries for " + region);
+      ListDeviceRegistriesResponse response = cloudIotService.projects().locations().registries()
+          .list(getLocationPath(region)).execute();
+      List<DeviceRegistry> deviceRegistries = response.getDeviceRegistries();
+      return ofNullable(deviceRegistries).orElseGet(ImmutableList::of).stream()
+          .map(DeviceRegistry::getId)
+          .collect(Collectors.toMap(item -> item, item -> region));
+    } catch (Exception e) {
+      throw new RuntimeException("While fetching registry cloud regions", e);
+    }
+  }
+
+  private String getRegistryPath(String registryId) {
+    String region = requireNonNull(registryCloudRegions.get(registryId),
+        "unknown region for registry " + registryId);
+    return format(REGISTRY_PATH_FORMAT, getLocationPath(region), registryId);
+  }
+
+  @Override
+  public void activate() {
+    try {
+      registries = cloudIotService.projects().locations().registries();
+    } catch (Exception e) {
+      throw new RuntimeException("While activating", e);
+    }
+  }
+
+  @Override
+  public void modifyConfig(String registryId, String deviceId, SubFolder subFolder,
+      String contents) {
+    // TODO: Need to implement checking of config version for concurrent operations.
+    String configString = ofNullable(fetchConfig(registryId, deviceId)).orElse(EMPTY_JSON);
+    Map<String, Object> configMap = toMap(configString);
+    configMap.put(subFolder.toString(), contents);
+    updateConfig(registryId, deviceId, stringify(configMap));
+  }
+
+  @Override
+  public void shutdown() {
+    registries = null;
+  }
+
   @Override
   public void updateConfig(String registryId, String deviceId, String config) {
     try {
@@ -110,22 +172,6 @@ public class GcpIotAccessProvider implements IotAccessProvider {
     } catch (Exception e) {
       throw new RuntimeException("While modifying device config", e);
     }
-  }
-
-  private String getProjectPath() {
-    return format(PROJECT_PATH_FORMAT, projectId);
-  }
-
-  private String getRegistryPath(String registryId) {
-    return format(REGISTRY_PATH_FORMAT, getProjectPath(), getCloudRegion(registryId), registryId);
-  }
-
-  private String getCloudRegion(String registryId) {
-    return registryCloudRegions.computeIfAbsent(registryId, key -> DEFAULT_CLOUD_REGION);
-  }
-
-  private String getDevicePath(String registryId, String deviceId) {
-    return format(DEVICE_PATH_FORMAT, getRegistryPath(registryId), deviceId);
   }
 }
 
