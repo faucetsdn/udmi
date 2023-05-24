2f188c02f72b833e3cd13900b4d7eb2d85a9e0c2
Trevor Pering <peringknife@google.com>
2023-05-23 21:03:57 -0700

    Fix pubber feature enumeration

--- pubber/src/main/java/daq/pubber/SupportedFeatures.java
+++ pubber/src/main/java/daq/pubber/SupportedFeatures.java
@@ -4,53 +4,38 @@ import static udmi.schema.Bucket.ENUMERATION;
 import static udmi.schema.Bucket.ENUMERATION_FAMILIES;
 import static udmi.schema.Bucket.ENUMERATION_FEATURES;
 import static udmi.schema.Bucket.ENUMERATION_POINTSET;
+import static udmi.schema.FeatureEnumeration.FeatureStage.ALPHA;
+import static udmi.schema.FeatureEnumeration.FeatureStage.BETA;
+import static udmi.schema.FeatureEnumeration.FeatureStage.STABLE;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Stack;
-import udmi.schema.FeatureEnumerationEvent;
-import udmi.schema.FeatureEnumerationEvent.Stage;
+import udmi.schema.Bucket;
+import udmi.schema.FeatureEnumeration;
+import udmi.schema.FeatureEnumeration.FeatureStage;
 
 /**
  * Static class to represent the features supported by this implementation.
  */
 public abstract class SupportedFeatures {
 
-  private static final Stack<FeatureEnumerationEvent> mapStack = new Stack<>();
+  private static final Map<String, FeatureEnumeration> FEATURES_MAP = new HashMap<>();
 
   static {
-    mapStack.push(new FeatureEnumerationEvent());
-    bucket(ENUMERATION.key(), Stage.STABLE, () -> {
-          bucket(ENUMERATION_FEATURES.key());
-          bucket(ENUMERATION_POINTSET.key(), Stage.BETA);
-          bucket(ENUMERATION_FAMILIES.key(), Stage.ALPHA);
-        }
-    );
+    add(ENUMERATION, STABLE);
+    add(ENUMERATION_FEATURES, BETA);
+    add(ENUMERATION_FEATURES, BETA);
+    add(ENUMERATION_POINTSET, BETA);
+    add(ENUMERATION_FAMILIES, ALPHA);
   }
 
-  public static Map<String, FeatureEnumerationEvent> getFeatures() {
-    return mapStack.peek().features;
+  private static void add(Bucket featureBucket, FeatureStage stage) {
+    FeatureEnumeration featureEnumeration = new FeatureEnumeration();
+    featureEnumeration.stage = stage;
+    FEATURES_MAP.put(featureBucket.value(), featureEnumeration);
   }
 
-  private static void bucket(String name) {
-    bucket(name, Stage.STABLE);
-  }
-
-  private static void bucket(String name, Stage stage) {
-    bucket(name, stage, null);
-  }
-
-  private static void bucket(String name, Stage stage, Runnable value) {
-    FeatureEnumerationEvent container = mapStack.peek();
-    container.features = Optional.ofNullable(container.features).orElseGet(HashMap::new);
-    FeatureEnumerationEvent features = container.features.computeIfAbsent(name,
-        key -> new FeatureEnumerationEvent());
-    features.stage = stage;
-    if (value != null) {
-      mapStack.push(features);
-      value.run();
-      mapStack.pop();
-    }
+  public static Map<String, FeatureEnumeration> getFeatures() {
+    return FEATURES_MAP;
   }
 }
