355f9d16a10f896829639bcef00642ad6f209f1c
Trevor Pering <peringknife@google.com>
2023-05-24 20:01:26 -0700

    Basic feature enumeration

--- pubber/src/main/java/daq/pubber/SupportedFeatures.java
+++ pubber/src/main/java/daq/pubber/SupportedFeatures.java
@@ -1,12 +1,8 @@
 package daq.pubber;
 
 import static udmi.schema.Bucket.ENUMERATION;
-import static udmi.schema.Bucket.ENUMERATION_FAMILIES;
 import static udmi.schema.Bucket.ENUMERATION_FEATURES;
-import static udmi.schema.Bucket.ENUMERATION_POINTSET;
-import static udmi.schema.FeatureEnumeration.FeatureStage.ALPHA;
 import static udmi.schema.FeatureEnumeration.FeatureStage.BETA;
-import static udmi.schema.FeatureEnumeration.FeatureStage.STABLE;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,11 +18,8 @@ public abstract class SupportedFeatures {
   private static final Map<String, FeatureEnumeration> FEATURES_MAP = new HashMap<>();
 
   static {
-    add(ENUMERATION, STABLE);
+    add(ENUMERATION, BETA);
     add(ENUMERATION_FEATURES, BETA);
-    add(ENUMERATION_FEATURES, BETA);
-    add(ENUMERATION_POINTSET, BETA);
-    add(ENUMERATION_FAMILIES, ALPHA);
   }
 
   private static void add(Bucket featureBucket, FeatureStage stage) {
