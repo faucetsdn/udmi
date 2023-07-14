41e4bc1c7d1fd1715efc556bd2eea1b6a5b31192
elsaidi <elsaidi@google.com>
2023-07-14 14:21:54 +0000

    system sequences

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/SystemSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/SystemSequences.java
@@ -7,6 +7,7 @@ import static udmi.schema.FeatureEnumeration.FeatureStage.BETA;
 
 import com.google.daq.mqtt.sequencer.Feature;
 import com.google.daq.mqtt.sequencer.SequenceBase;
+import java.util.Map;
 import org.junit.AssumptionViolatedException;
 import org.junit.Test;
 
@@ -20,11 +21,12 @@ public class SystemSequences extends SequenceBase {
    */
   @Test(timeout = THREE_MINUTES_MS)
   @Feature(stage = BETA, bucket = SYSTEM)
-  @Summary("device publishes pointset events")
+  @Summary("device publishes correct make and model information in state messages")
   public void state_make_model() {
     // Inspect make and model rather than complete hardware block
     String expected_make = catchToNull(() -> deviceMetadata.system.hardware.make);
     String expected_model = catchToNull(() -> deviceMetadata.system.hardware.model);
+
     if (expected_make == null || expected_model == null) {
       throw new AssumptionViolatedException(
           "make and model not defined in metadata"
@@ -34,9 +36,47 @@ public class SystemSequences extends SequenceBase {
     String actual_make = catchToNull(() -> deviceState.system.hardware.make);
     String actual_model = catchToNull(() -> deviceState.system.hardware.model);
 
-    checkThat("make and model match",
-        () -> expected_make.equals(actual_make) && expected_model.equals(actual_model)
+    checkThat(format("make in state `%s` matches make in metadata `%s`",
+            actual_make, expected_make),
+        () -> expected_make.equals(actual_make)
     );
+
+    checkThat(format("model in state `%s` matches model in metadata `%s`",
+            actual_model, expected_model),
+        () -> expected_model.equals(actual_model)
+    );
+  }
+
+  /**
+   * Checks system.software.* keys in metadata are:
+   *  (1) in state message
+   *  (2) match
+   *  Because a device may report a lot information in the state message than that which is user
+   *  controllable.
+   *  For example, if firmware v1.1 is installed, the device may report the version of all packages
+   *  within this firmware
+   */
+  @Test(timeout = THREE_MINUTES_MS)
+  @Feature(stage = BETA, bucket = SYSTEM)
+  @Summary("device publishes correct software information in state messages")
+  public void state_software() {
+
+    Map metadata_software = catchToNull(() -> deviceMetadata.system.software);
+    if ( metadata_software == null ) {
+      throw new AssumptionViolatedException(
+          "software not defined in metadata"
+      );
+    }
+    Map state_software = catchToNull(() -> deviceState.system.software);
+
+    checkThat("software in state matches software in metadata",
+        () -> state_software != null &&
+            state_software.entrySet().containsAll(metadata_software.entrySet()
+        )
+    );
+
+
+
   }
 
 }
