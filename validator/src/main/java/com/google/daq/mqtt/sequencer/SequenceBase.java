e717440d4b22e7ef33747d9bc9d79870b2bf3386
Trevor Pering <peringknife@google.com>
2023-07-20 14:26:50 -0700

    Fix time reporting

--- validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
@@ -721,7 +721,7 @@ public class SequenceBase {
   private void recordSchemaValidations(Description description) {
     // Ensure that enough time has passed to capture event messages for schema validation.
     if (waitTimeRemainingSec() > 0) {
-      debug("waiting %ss for more messages...", Long.toString(waitTimeRemainingSec()));
+      debug(format("waiting %ds for more messages...", waitTimeRemainingSec()));
       whileDoing(format("minimum test time of %ss", MINIMUM_TEST_SEC),
           () -> messageEvaluateLoop(() -> waitTimeRemainingSec() > 0));
     }
@@ -1213,7 +1213,7 @@ public class SequenceBase {
   }
 
   private long waitTimeRemainingSec() {
-    return (System.currentTimeMillis() - startCaptureTime) / 1000;
+    return (System.currentTimeMillis() - startCaptureTime) / 1000 - MINIMUM_TEST_SEC;
   }
 
   private String timeSinceStart() {
