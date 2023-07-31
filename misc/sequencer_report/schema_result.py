55231d011be030405e558eb388f76bff0ea65bad
elsaidi <elsaidi@google.com>
2023-07-31 10:39:32 +0000

    remove misc and maybe fix sequencer logic

--- misc/sequencer_report/schema_result.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-#ESULT pass discovery.scan periodic_scan ALPHA 5 Sequence complete
-@dataclass(eq=True, order=True)
-class SchemaResult:
-    schema: str = ''
-    result: str = ''
-   
