bbaed8c51cb1c2ad5566d0fd3f5499e14fa40f08
Trevor Pering <peringknife@google.com>
2023-08-05 23:58:21 -0700

    Simplified version

--- etc/actions_init.sh
+++ etc/actions_init.sh
@@ -14,10 +14,4 @@ function fail {
 export UDMI_BIN=$HOME/udmi/bin
 
 UDMI_WORKFLOW=$HOME/udmi/etc/site_model_workflow.yaml
-MODEL_WORKFLOW=.github/workflows/testing.yaml
 
-pwd
-ls -la
-find . -name testing.yaml
-ls -l $UDMI_WORKFLOW $MODEL_WORKFLOW
-! diff $UDMI_WORKFLOW $MODEL_WORKFLOW || fail UDMI and Model workflows differ
