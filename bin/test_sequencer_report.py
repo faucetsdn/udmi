8bf82bb02e507be24d5fa45f9353bd6dcf6fc9a5
Noureddine <noureddine@saidi.uk>
2023-10-12 14:31:00 +0000

    fix lint errors and sequencer cache checking

--- bin/test_sequencer_report.py
+++ bin/test_sequencer_report.py
@@ -1,8 +1,11 @@
 import unittest
 import json
-from  sequencer_report import Sequence, SequenceStep
+from importlib import util, machinery
 from dataclasses import dataclass
 
+spec = util.spec_from_loader("sequence_report", machinery.SourceFileLoader("sequence_report", "bin/sequencer_report"))
+seq = util.module_from_spec(spec)
+spec.loader.exec_module(seq)
 
 
 SAMPLE_SEQUENCE = """
@@ -28,7 +31,8 @@ Check that the device correctly handles an extra out-of-schema field
 1. Step 2
 """
 
-SAMPLE_EXPECTED = """✓   1. Step 1
+SAMPLE_EXPECTED = """
+✓   1. Step 1
 ✓   2. Multistep 2
       * line a
       * line b
@@ -37,7 +41,8 @@ SAMPLE_EXPECTED = """✓   1. Step 1
 ✕   3. Step 3 ✕
 ----------------
 
-    4. Step 4"""
+    4. Step 4
+"""
 
 REAL_SEQUENCE = """
 ## empty_enumeration (PREVIEW)
@@ -79,28 +84,28 @@ class SampleTestResult:
 class TestSequencerReport(unittest.TestCase):
 
   def test_sequence_get_steps(self):
-    steps = Sequence.get_steps(SAMPLE_SEQUENCE)
+    steps = seq.Sequence.get_steps(SAMPLE_SEQUENCE)
     #print(json.dumps(steps))
     self.assertEqual(steps[0][1], "    * Substep 1")
     self.assertEqual(steps[2][0], "1. Step `3`")
 
   def test_indent_with_prefix(self):
-    formatted = Sequence.indent("some string", 5, '>')
+    formatted = seq.Sequence.indent("some string", 5, '>')
     self.assertEqual(formatted, ">    some string")
 
   def test_longest_line(self):
     lines = ['a', 'abc', 'abcdef']
-    self.assertEqual(Sequence.longest_line_length(lines), 6)
+    self.assertEqual(seq.Sequence.longest_line_length(lines), 6)
 
   def test_failing_step(self):
     # method doesn't actually care what the contents of the list
     # so just use a fixed list
     ref = ['Step 1', 'Step 2', 'Step 3', ' Step 4']
     act = ['Step 1', 'Step 2']
-    failing_step = Sequence.failing_step(ref, act)
+    failing_step = seq.Sequence.failing_step(ref, act)
     self.assertEqual(failing_step, 1)
 
-    passed_all = Sequence.failing_step(ref, ref)
+    passed_all = seq.Sequence.failing_step(ref, ref)
     # if non failed is the index of the last step
     self.assertEqual(passed_all, 3)
   
@@ -109,46 +114,45 @@ class TestSequencerReport(unittest.TestCase):
     ref = ['Step 1', 'Step 2', 'Step 3', ' Step 4']
     act = ['Step 1', 'Step 2']
 
-    done, fail, todo = Sequence.classify_steps(ref, act)
+    done, fail, todo = seq.Sequence.classify_steps(ref, act)
     self.assertEqual(len(done), 1)
     self.assertEqual(len(todo), 2)
 
-    done, fail, todo = Sequence.classify_steps(ref, ref, 'pass')
+    done, fail, todo = seq.Sequence.classify_steps(ref, ref, 'pass')
     self.assertEqual(len(done), 4)
     self.assertEqual(len(todo), 0)
 
-    done, fail, todo = Sequence.classify_steps(ref, ref, 'fail')
+    done, fail, todo = seq.Sequence.classify_steps(ref, ref, 'fail')
     self.assertEqual(len(done), 3)
 
   def test_class_single(self):
     ref = [['Step 1'], ['Step 2'], ['Step 3'], [' Step 4']]
-    done, fail, todo = Sequence.classify_steps(ref, ref[0], 'fail')
-    print(done)
-    print(fail)
-    print(todo)
+    done, fail, todo = seq.Sequence.classify_steps(ref, ref[0], 'fail')
+
 
   def test_formatter(self):
     ref = [['1. Step 1'], ['1. Multistep 2', '  * line a', '  * line b'], ['1. Step 3'], ['1. Step 4']]
     act = [['1. Step 1'], ['1. Multistep 2', '  * line a', '  * line b']]
 
-    formatted = "\n".join(Sequence.format(ref, act, SampleTestResult()))
+    formatted = seq.Sequence.format(ref, act, SampleTestResult())
+    print(formatted)
+    print(SAMPLE_EXPECTED)
     self.assertEqual(formatted, SAMPLE_EXPECTED)
 
   def test_formatter_real(self):
-    ref = Sequence.get_steps(REAL_SEQUENCE)
-    act = Sequence.get_steps(REAL_SEQUENCE_PARTIAL)
+    ref = seq.Sequence.get_steps(REAL_SEQUENCE)
+    act = seq.Sequence.get_steps(REAL_SEQUENCE_PARTIAL)
 
-    a = Sequence.format(ref, act, SampleTestResult())
+    a = seq.Sequence.format(ref, act, SampleTestResult())
     
     #print('\n'.join(a))
         
   def test_sequence(self):
-    seq = Sequence(SampleTestResult, 'validator/sequences', 'sites/udmi_site_model/out/devices/AHU-1/tests')
-    print(str(seq))
+    output = seq.Sequence(SampleTestResult, 'validator/sequences', 'sites/udmi_site_model/out/devices/AHU-1/tests')
 
 
   def test_single_step(self):
-      steps = Sequence.get_steps(SAMPLE_SEQUENCE)
+      steps = seq.Sequence.get_steps(SAMPLE_SEQUENCE)
       #print(json.dumps(steps))
       self.assertEqual(steps[0][1], "    * Substep 1")
       self.assertEqual(steps[2][0], "1. Step `3`")
