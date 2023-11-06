2b4d71b6f060b5b9ec846d2b36c32b4c2b5dc8f5
Trevor Pering <peringknife@google.com>
2023-11-06 13:30:08 -0500

    Fix no-namespace

--- common/src/main/java/com/google/udmi/util/Common.java
+++ common/src/main/java/com/google/udmi/util/Common.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
+import joptsimple.internal.Strings;
 
 /**
  * Collection of common constants and minor utilities.
@@ -161,6 +162,6 @@ public abstract class Common {
   }
 
   public static String getNamespacePrefix(String udmiNamespace) {
-    return ofNullable(udmiNamespace).map(x -> x + PREFIX_SEPARATOR).orElse("");
+    return Strings.isNullOrEmpty(udmiNamespace) ? "" : udmiNamespace + PREFIX_SEPARATOR;
   }
 }
