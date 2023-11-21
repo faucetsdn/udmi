9efc2dd638e5124247fac5cbd951da907b1c4828
Trevor <grafnu@users.noreply.github.com>
2023-11-21 07:00:06 -0400

    Fixes for Pubber startup errors (#772)

--- validator/src/main/java/com/google/daq/mqtt/util/ObjectDiffEngine.java
+++ validator/src/main/java/com/google/daq/mqtt/util/ObjectDiffEngine.java
@@ -116,7 +116,9 @@ public class ObjectDiffEngine {
       List<String> updates) {
     right.forEach((key, value) -> {
       String describedKey = describedKey(prefix, key);
-      String describedValue = describeValue(prefix, key, semanticValue(value));
+      String raw = describeValue(prefix, key, semanticValue(value));
+      int index = raw.indexOf('\n');
+      String describedValue = index < 0 ? raw : (raw.substring(0, index) + "...");
       if (left != null && left.containsKey(key)) {
         Object leftValue = left.get(key);
         if (SemanticValue.equals(value, leftValue)) {
