667ef14df300140d7bf1102de26e900499d927dc
Trevor Pering <peringknife@google.com>
2023-11-22 09:05:49 -0400

    Fix typo

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/SystemSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/SystemSequences.java
@@ -9,7 +9,6 @@ import com.google.daq.mqtt.sequencer.Feature;
 import com.google.daq.mqtt.sequencer.SequenceBase;
 import com.google.daq.mqtt.sequencer.Summary;
 import java.util.Map;
-import org.junit.AssumptionViolatedException;
 import org.junit.Test;
 
 /**
@@ -18,7 +17,7 @@ import org.junit.Test;
 public class SystemSequences extends SequenceBase {
 
   /**
-   * Simple check that device publishes pointset events.
+   * Simple check that device contains appropriate make/model descriptions.
    */
   @Test(timeout = THREE_MINUTES_MS)
   @Feature(stage = BETA, bucket = SYSTEM)
@@ -29,11 +28,12 @@ public class SystemSequences extends SequenceBase {
         () -> deviceMetadata.system.hardware.make,
         "make not in metadata"
       );
-    info("expected make" + expectedMake);
+
     String expectedModel = ifCatchNullSkipTest(
         () -> deviceMetadata.system.hardware.model,
         "model not in metadata"
     );
+
     String actualMake = catchToNull(() -> deviceState.system.hardware.make);
     String actualModel = catchToNull(() -> deviceState.system.hardware.model);
     checkThat("make and model in state matches make in metadata",
@@ -46,27 +46,20 @@ public class SystemSequences extends SequenceBase {
    * Checks system.software.* keys in metadata are:
    *  (1) in state message
    *  (2) match
-   *  Because a device may report a lot information in the state message than that which is user
-   *  controllable.
-   *  For example, if firmware v1.1 is installed, the device may report the version of all packages
-   *  within this firmware in addition to firmware v1.1
    */
   @Test(timeout = THREE_MINUTES_MS)
   @Feature(stage = BETA, bucket = SYSTEM)
   @Summary("Check that a device publishes correct software information in state messages")
   public void state_software() {
 
-    Map expectedSoftware = ifCatchNullSkipTest(
+    Map<String, String> expectedSoftware = ifCatchNullSkipTest(
         () -> deviceMetadata.system.software,
         "software not defined in metadata");
 
-    Map actualSoftware = catchToNull(() -> deviceState.system.software);
+    Map<String, String> actualSoftware = deviceState.system.software;
 
     checkThat("software in metadata matches state",
-        () -> actualSoftware != null
-            && actualSoftware.entrySet().containsAll(expectedSoftware.entrySet()
-        )
-    );
+        () -> actualSoftware.entrySet().equals(expectedSoftware.entrySet()));
   }
 
 }
