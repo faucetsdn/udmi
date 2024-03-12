95380c6c12dd7b64b962cc0980e219d54e477ea9
Trevor Pering <peringknife@google.com>
2024-03-11 17:50:35 -0700

    Add basic implementation of IpProvider

--- pubber/src/main/java/daq/pubber/VendorProvider.java
+++ pubber/src/main/java/daq/pubber/VendorProvider.java
@@ -1,10 +1,12 @@
 package daq.pubber;
 
+import udmi.schema.Common.ProtocolFamily;
 import udmi.schema.PubberConfiguration;
 
 public class VendorProvider extends ManagerBase implements LocalnetProvider {
 
-  public VendorProvider(ManagerHost host, PubberConfiguration pubberConfiguration) {
+  public VendorProvider(ManagerHost host, ProtocolFamily family,
+      PubberConfiguration pubberConfiguration) {
     super(host, pubberConfiguration);
   }
 
