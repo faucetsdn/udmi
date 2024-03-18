11197c81b1a3f0022adf8d6d3a888da8db2fe5a2
Trevor Pering <peringknife@google.com>
2024-03-19 07:19:02 +1300

    Update discovery path

--- udmis/src/main/java/com/google/bos/udmi/service/core/MappingAgent.java
+++ udmis/src/main/java/com/google/bos/udmi/service/core/MappingAgent.java
@@ -3,11 +3,20 @@ package com.google.bos.udmi.service.core;
 import static com.google.api.client.util.Preconditions.checkState;
 import static com.google.udmi.util.GeneralUtils.catchToElse;
 import static com.google.udmi.util.GeneralUtils.friendlyStackTrace;
+import static com.google.udmi.util.GeneralUtils.ifNotNullGet;
+import static com.google.udmi.util.GeneralUtils.ifTrueGet;
 import static com.google.udmi.util.GeneralUtils.ignoreValue;
+import static com.google.udmi.util.JsonUtil.stringifyTerse;
+import static com.google.udmi.util.MetadataMapKeys.UDMI_DISCOVERED_FROM;
+import static com.google.udmi.util.MetadataMapKeys.UDMI_DISCOVERED_WITH;
+import static com.google.udmi.util.MetadataMapKeys.UDMI_ONBOARD_UNTIL;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 import static udmi.schema.CloudModel.Operation.BIND;
 
+import com.google.udmi.util.JsonUtil;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,11 +36,20 @@ import udmi.schema.Envelope;
 public class MappingAgent extends ProcessorBase {
 
   private static final String EXPECTED_DEVICE_FORMAT = "%s-%s";
+  private static final Instant DEFALUT_ONBOARD_MARKER = Instant.ofEpochMilli(0);
+  private static final Duration ONBOARDING_MAX_WINDOW = Duration.ofDays(7);
 
   private final Map<String, CloudModel> scanAgent = new ConcurrentHashMap<>();
 
   public MappingAgent(EndpointConfiguration config) {
     super(config);
+  }
+
+  private static boolean shouldOnboard(Date generation, CloudModel cloudModel) {
+    String timestamp = cloudModel.metadata.get(UDMI_ONBOARD_UNTIL);
+    Date latest = Date.from(ifNotNullGet(timestamp, JsonUtil::getInstant, DEFALUT_ONBOARD_MARKER));
+    Date earliest = Date.from(latest.toInstant().minus(ONBOARDING_MAX_WINDOW));
+    return !generation.after(latest) && !generation.before(earliest);
   }
 
   private void bindDeviceToGateway(String registryId, String proxyId, String gatewayId) {
@@ -42,12 +60,16 @@ public class MappingAgent extends ProcessorBase {
     iotAccess.modelResource(registryId, gatewayId, model);
   }
 
-  private void createDeviceEntry(String registryId, String expectedId) {
+  private void createDeviceEntry(String registryId, String expectedId, String gatewayId,
+      Envelope envelope, DiscoveryEvent discoveryEvent) {
     CloudModel deviceModel = new CloudModel();
     deviceModel.operation = Operation.CREATE;
     deviceModel.blocked = true;
+    deviceModel.metadata.put(UDMI_DISCOVERED_FROM, stringifyTerse(envelope));
+    deviceModel.metadata.put(UDMI_DISCOVERED_WITH, stringifyTerse(discoveryEvent));
     catchToElse(ignoreValue(iotAccess.modelResource(registryId, expectedId, deviceModel)),
         e -> error("Error creating device (exists but not bound?): " + friendlyStackTrace(e)));
+    bindDeviceToGateway(registryId, expectedId, gatewayId);
   }
 
   private CloudModel getCloudModel(String deviceRegistryId, String gatewayId) {
@@ -60,13 +82,15 @@ public class MappingAgent extends ProcessorBase {
     CloudModel cloudModel = getCloudModel(deviceRegistryId, gatewayId);
     if (!generation.equals(cloudModel.timestamp)) {
       String gatewayKey = format(EXPECTED_DEVICE_FORMAT, deviceRegistryId, gatewayId);
-      info("New scan %s generation %s, fetching current model...", gatewayKey, generation);
       CloudModel fetchedModel = iotAccess.fetchDevice(deviceRegistryId, gatewayId);
       checkState(fetchedModel.resource_type == Resource_type.GATEWAY, "Device is not a gateway");
-      cloudModel.device_ids = fetchedModel.device_ids;
       cloudModel.timestamp = generation;
+      cloudModel.device_ids = fetchedModel.device_ids;
+      cloudModel.metadata = fetchedModel.metadata;
+      info("New scan %s generation %s, onboarding %s", gatewayKey, generation,
+          shouldOnboard(generation, cloudModel));
     }
-    return cloudModel.device_ids;
+    return ifTrueGet(shouldOnboard(generation, cloudModel), cloudModel.device_ids, null);
   }
 
   /**
@@ -86,8 +110,7 @@ public class MappingAgent extends ProcessorBase {
       debug("Scan %s/%s device %s already registered", registryId, gatewayId, expectedId);
     } else {
       notice("Scan %s/%s device %s missing, creating", registryId, expectedId, gatewayId);
-      createDeviceEntry(registryId, expectedId);
-      bindDeviceToGateway(registryId, expectedId, gatewayId);
+      createDeviceEntry(registryId, expectedId, gatewayId, envelope, discoveryEvent);
       deviceIds.put(expectedId, new CloudModel());
     }
   }
