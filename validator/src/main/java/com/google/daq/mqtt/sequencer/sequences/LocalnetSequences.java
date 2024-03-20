a01e9176950635d6e57f7d19cbb000ebcd968784
Trevor Pering <peringknife@google.com>
2024-03-20 22:11:59 +1300

    Fix address family logic

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/LocalnetSequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/LocalnetSequences.java
@@ -7,11 +7,8 @@ import static udmi.schema.FeatureDiscovery.FeatureStage.PREVIEW;
 
 import com.google.daq.mqtt.sequencer.Feature;
 import com.google.daq.mqtt.sequencer.SequenceBase;
-import java.util.HashMap;
 import org.junit.Test;
 import udmi.schema.Common.ProtocolFamily;
-import udmi.schema.FamilyLocalnetConfig;
-import udmi.schema.LocalnetConfig;
 
 /**
  * Validate localnet related functionality.
@@ -22,10 +19,8 @@ public class LocalnetSequences extends SequenceBase {
     final String expected = ifNullSkipTest(
         catchToNull(() -> deviceMetadata.localnet.families.get(family).addr),
         format("No %s address defined in metadata", family));
-    deviceConfig.localnet = new LocalnetConfig();
-    deviceConfig.localnet.families = new HashMap<>();
-    deviceConfig.localnet.families.computeIfAbsent(family, key -> new FamilyLocalnetConfig());
-    untilTrue("localnet families available", () -> deviceState.localnet.families.size() > 0);
+    untilTrue(format("localnet family state %s available", family),
+        () -> deviceState.localnet.families.containsKey(family));
     String actual = catchToNull(() -> deviceState.localnet.families.get(family).addr);
     checkThat(format("device family %s address matches", family),
         () -> expected.equals(actual));
