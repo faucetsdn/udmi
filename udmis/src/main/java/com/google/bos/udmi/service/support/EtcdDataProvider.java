ece346e374ec0709aa98214df7084d2cb69a39b8
Trevor Pering <peringknife@google.com>
2024-04-10 22:50:34 -0700

    Working data client redirection

--- udmis/src/main/java/com/google/bos/udmi/service/support/EtcdDataProvider.java
+++ udmis/src/main/java/com/google/bos/udmi/service/support/EtcdDataProvider.java
@@ -1,5 +1,6 @@
 package com.google.bos.udmi.service.support;
 
+import static com.google.api.client.util.Preconditions.checkState;
 import static com.google.udmi.util.GeneralUtils.ifNotNullThen;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
@@ -11,6 +12,7 @@ import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.Client;
 import io.etcd.jetcd.cluster.Member;
 import java.net.URI;
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -23,7 +25,10 @@ import udmi.schema.IotAccess;
  */
 public class EtcdDataProvider extends ContainerBase implements IotDataProvider {
 
+  private static final String EXPECTED_PREFIX = "http://";
+  private static final String RESULTING_PREFIX = "ip:///";
   private static final long QUERY_TIMEOUT_SEC = 10;
+  private static final Duration CONNECT_TIMEOUT = Duration.ofSeconds(QUERY_TIMEOUT_SEC);
   private static final ByteSequence CONNECTED_KEY =
       getByteSequence(format("clients/%s", DistributorPipe.clientId));
   private final IotAccess config;
@@ -40,21 +45,26 @@ public class EtcdDataProvider extends ContainerBase implements IotDataProvider {
   }
 
   private Client initializeClient() {
-    String target = format("ip://%s", requireNonNull(config.project_id, "undefined project_id"));
+    String target = requireNonNull(config.project_id, "undefined project_id");
     try (Client tmpClient = Client.builder().target(target).build()) {
       debug("Connecting to target %s to glean client list", target);
       List<Member> members =
           tmpClient.getClusterClient().listMember().get(QUERY_TIMEOUT_SEC, TimeUnit.SECONDS)
               .getMembers();
-      Set<URI> uris = members.stream().flatMap(member -> member.getClientURIs().stream())
-          .collect(Collectors.toSet());
-      String targets = uris.stream().map(URI::toString).collect(Collectors.joining(","));
+      Set<String> uris = members.stream().flatMap(member -> member.getClientURIs().stream())
+          .map(URI::toString).collect(Collectors.toSet());
+      checkState(!uris.isEmpty(), "no member uris found");
+      checkState(uris.stream().allMatch(uri -> uri.startsWith(EXPECTED_PREFIX)),
+          "inconsistent prefix");
+      String collected = uris.stream().map(uri -> uri.substring(EXPECTED_PREFIX.length()))
+          .collect(Collectors.joining(","));
+      String targets = RESULTING_PREFIX + collected;
       debug("Gleaned client targets " + targets);
-      Client client = Client.builder().target(targets).build();
+      Client client = Client.builder().target(targets).connectTimeout(CONNECT_TIMEOUT).build();
       String timestamp = GeneralUtils.getTimestamp();
       client.getKVClient().put(CONNECTED_KEY, getByteSequence(timestamp))
           .get(QUERY_TIMEOUT_SEC, TimeUnit.SECONDS);
-      debug("Updated client %s at %s", CONNECTED_KEY, timestamp);
+      debug("Updated client %s with %s", CONNECTED_KEY, timestamp);
       return client;
     } catch (Exception e) {
       throw new RuntimeException("While connecting initial client " + target, e);
