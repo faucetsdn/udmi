e44a8e9c6fbbf0024815bdaaf5550ca9b1dc5d20
Trevor Pering <peringknife@google.com>
2024-04-13 08:42:39 -0700

    Add basics for implicit iot access provider

---
+++ udmis/src/main/java/com/google/bos/udmi/service/access/ImplicitIotAccessProvider.java
@@ -0,0 +1,66 @@
+package com.google.bos.udmi.service.access;
+
+import java.util.Map.Entry;
+import udmi.schema.CloudModel;
+import udmi.schema.Envelope.SubFolder;
+import udmi.schema.IotAccess;
+
+public class ImplicitIotAccessProvider extends IotAccessBase {
+
+  public ImplicitIotAccessProvider(IotAccess iotAccess) {
+    super(iotAccess);
+  }
+
+  @Override
+  public void activate() {
+    // TODO: Get database connection here
+    super.activate();
+    // TODO: Activate reflector pipe here
+  }
+
+  @Override
+  public Entry<Long, String> fetchConfig(String registryId, String deviceId) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public CloudModel fetchDevice(String deviceRegistryId, String deviceId) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public String fetchRegistryMetadata(String registryId, String metadataKey) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public String fetchState(String deviceRegistryId, String deviceId) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public boolean isEnabled() {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public CloudModel listDevices(String deviceRegistryId) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public CloudModel modelResource(String deviceRegistryId, String deviceId, CloudModel cloudModel) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public void sendCommandBase(String registryId, String deviceId, SubFolder folder,
+      String message) {
+    throw new RuntimeException("Not yet implemented");
+  }
+
+  @Override
+  public String updateConfig(String registryId, String deviceId, String config, Long version) {
+    throw new RuntimeException("Not yet implemented");
+  }
+}
