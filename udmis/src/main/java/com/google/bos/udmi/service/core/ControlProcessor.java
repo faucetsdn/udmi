30f83a52da249bcb811eaa2f6d05dc06570e6e20
Trevor Pering <peringknife@google.com>
2024-05-09 10:09:57 -0700

    Generate cron test messages

--- udmis/src/main/java/com/google/bos/udmi/service/core/ControlProcessor.java
+++ udmis/src/main/java/com/google/bos/udmi/service/core/ControlProcessor.java
@@ -1,8 +1,11 @@
 package com.google.bos.udmi.service.core;
 
+import static com.google.udmi.util.JsonUtil.isoConvert;
+
 import com.google.bos.udmi.service.pod.UdmiServicePod;
 import udmi.schema.CloudQuery;
 import udmi.schema.EndpointConfiguration;
+import udmi.schema.UdmiConfig;
 
 /**
  * Handle the control processor stream for UDMI utility tool clients.
@@ -28,5 +31,18 @@ public class ControlProcessor extends ProcessorBase {
   @MessageHandler
   public void cloudQueryHandler(CloudQuery query) {
     CloudQueryHandler.processQuery(this, query);
+  }
+
+  /**
+   * Handle a udmi config command.
+   */
+  @MessageHandler
+  public void udmiConfigHandler(UdmiConfig config) {
+    if (config.timestamp != null) {
+      debug("Processing UdmiConfig " + isoConvert(config.timestamp));
+      UdmiConfig udmiConfig = UdmiServicePod.getUdmiConfig(null);
+      config.setup = udmiConfig.setup;
+      publish(config);
+    }
   }
 }
