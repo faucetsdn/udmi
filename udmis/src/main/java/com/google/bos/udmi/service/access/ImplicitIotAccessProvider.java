f32f7ff3cbb92d0ff13a3df6e8402a96e20605b5
Trevor Pering <peringknife@google.com>
2024-06-15 06:30:19 -0700

    Adding state diagnostics

--- udmis/src/main/java/com/google/bos/udmi/service/access/ImplicitIotAccessProvider.java
+++ udmis/src/main/java/com/google/bos/udmi/service/access/ImplicitIotAccessProvider.java
@@ -233,7 +233,9 @@ public class ImplicitIotAccessProvider extends IotAccessBase {
 
   @Override
   public String fetchState(String registryId, String deviceId) {
-    return registryDeviceRef(registryId, deviceId).get(LAST_STATE_KEY);
+    String stateBlob = registryDeviceRef(registryId, deviceId).get(LAST_STATE_KEY);
+    debug("TAP Fetching state %s/%s %s", registryId, deviceId, stateBlob);
+    return stateBlob;
   }
 
   @Override
@@ -265,6 +267,7 @@ public class ImplicitIotAccessProvider extends IotAccessBase {
 
   @Override
   public void saveState(String registryId, String deviceId, String stateBlob) {
+    debug("TAP Saving state %s/%s %s", registryId, deviceId, stateBlob);
     registryDeviceRef(registryId, deviceId).put(LAST_STATE_KEY, stateBlob);
   }
 
