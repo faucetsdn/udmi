7fae4b960602d015d3cb41340fa006d77c109607
Noureddine El Saidi <elsaidi@google.com>
2024-09-11 14:32:53 +0100

    add docker container (untested)

--- misc/discoverynode/src/main.py
+++ misc/discoverynode/src/main.py
@@ -1,20 +1,43 @@
+import argparse
 import logging
 import sys
 import time
 import tomllib
-import argparse
 import warnings
 # filter deprecation notice from SCAPY import
-warnings.filterwarnings(action='ignore', module='.*ipsec.*')
+warnings.filterwarnings(action="ignore", module=".*ipsec.*")
 
 import udmi.core
 import udmi.publishers.mqtt
 
+
+def or_required_from_env(key: str) -> dict[str, str | int | bool]:
+  """Used in argparse to return a non-optional value from env vars.
+
+  Unpack return value as argument for argparse.ArgumentParser.add_argument.
+
+  Args:
+    key: environmental variable name.
+
+  Returns:
+    value from env var if set or indicator that field is required.
+  """
+  try:
+    return {"default": os.environ[key]}
+  except KeyError:
+    return {"required": True}
+
+
 def get_arguments():
-  parser = argparse.ArgumentParser(description='Start UDMI Discovey Client')
-  parser.add_argument('config_file', type=str,
-                      help='path to config file')
+  parser = argparse.ArgumentParser(description="Start UDMI Discovey Client")
+  parser.add_argument(
+      "config_file",
+      type=str,
+      help="path to config file",
+      **or_required_from_env("CONFIG_FILE"),
+  )
   return parser.parse_args()
+
 
 def load_config_from_file(file_name: str):
   with open(file_name, "rb") as f:
@@ -53,7 +76,9 @@ def main():
   )
 
   udmi_client = udmi.core.UDMI(
-      publisher=mclient, topic_prefix=f'/devices/{config["mqtt"]["device_id"]}/', config=config
+      publisher=mclient,
+      topic_prefix=f'/devices/{config["mqtt"]["device_id"]}/',
+      config=config,
   )
 
   mclient.set_config_callback(udmi_client.config_handler)
