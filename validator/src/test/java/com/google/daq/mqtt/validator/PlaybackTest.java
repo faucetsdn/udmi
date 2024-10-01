2bf9375abe3c615cfbf295f1f28c39405388c4e9
Trevor Pering <peringknife@google.com>
2024-09-30 19:21:14 -0700

    Add new lengthy test

--- validator/src/test/java/com/google/daq/mqtt/validator/PlaybackTest.java
+++ validator/src/test/java/com/google/daq/mqtt/validator/PlaybackTest.java
@@ -24,6 +24,7 @@ import udmi.schema.ValidationState;
 public class PlaybackTest extends TestBase {
 
   public static final String SIMPLE_TRACE_DIR = "../tests/traces/simple";
+  public static final String LENGTHY_TRACE_DIR = "../tests/traces/lengthy";
   private static final List<String> TRACE_DEVICES = List.of("--", "AHU-22", "SNS-4", "XXX", "YYY");
 
   @Test
@@ -70,6 +71,45 @@ public class PlaybackTest extends TestBase {
         .filter(bundle -> bundle.deviceId.equals(deviceId))
         .map(bundle -> asValidationEvents(bundle.message))
         .collect(Collectors.toList());
+  }
+
+  @Test
+  public void notMissingDevice() {
+    MessageReadingClient client = validateTrace(LENGTHY_TRACE_DIR);
+    assertEquals("trace message count", 3, client.messageCount);
+    List<OutputBundle> outputMessages = client.getOutputMessages();
+    OutputBundle lastBundle = outputMessages.get(outputMessages.size() - 1);
+    ValidationState finalReport = asValidationState(lastBundle.message);
+    try {
+      assertEquals("correct devices", 1, finalReport.summary.correct_devices.size());
+      assertEquals("extra devices", 0, finalReport.summary.extra_devices.size());
+      assertEquals("missing devices", 0, finalReport.summary.missing_devices.size());
+      assertEquals("error devices", 3, finalReport.summary.error_devices.size());
+      assertEquals("device summaries", 4, finalReport.devices.size());
+      assertEquals("AHU-1 status level", (Object) INFO.value(),
+          finalReport.devices.get("AHU-1").status.level);
+      assertEquals("AHU-22 status category", Category.VALIDATION_DEVICE_SCHEMA,
+          finalReport.devices.get("AHU-22").status.category);
+      assertEquals("SNS-4 status", Category.VALIDATION_DEVICE_MULTIPLE,
+          finalReport.devices.get("SNS-4").status.category);
+
+      List<ValidationEvents> deviceReports = reports(outputMessages, "AHU-1");
+
+      ValidationEvents firstReport = deviceReports.get(0);
+      assertEquals("missing points", 1, firstReport.pointset.missing.size());
+      String missingPointName = firstReport.pointset.missing.get(0);
+      assertEquals("missing point", FILTER_DIFFERENTIAL_PRESSURE_SETPOINT, missingPointName);
+      assertEquals("extra points", 0, firstReport.pointset.extra.size());
+      assertEquals("device status", (Integer) Level.ERROR.value(), firstReport.status.level);
+
+      ValidationEvents lastReport = deviceReports.get(deviceReports.size() - 1);
+      assertEquals("missing points", 0, lastReport.pointset.missing.size());
+      assertEquals("extra points", 0, lastReport.pointset.extra.size());
+      assertEquals("device status level", (Object) INFO.value(), lastReport.status.level);
+    } catch (Throwable e) {
+      outputMessages.forEach(message -> System.err.println(JsonUtil.stringify(message)));
+      throw e;
+    }
   }
 
   @Test
