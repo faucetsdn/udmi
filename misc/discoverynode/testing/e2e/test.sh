daeb875210aace4ce41f6966df693ff7884ed155
Noureddine <noureddine@saidi.uk>
2024-10-11 18:44:52 +0000

    enable capture

--- misc/discoverynode/testing/e2e/test.sh
+++ misc/discoverynode/testing/e2e/test.sh
@@ -33,4 +33,4 @@ bash $ROOT_DIR/../docker/discovery_node/build.sh
 
 docker network create --subnet=192.168.11.0/24 --ip-range=192.168.11.0/24 --gateway=192.168.11.254 discoverynode-network || true
 
-python3 -m pytest -v --capture=no --log-cli-level=INFO $ROOT_DIR/e2e_test.py
+python3 -m pytest -v --log-cli-level=INFO $ROOT_DIR/e2e_test.py
