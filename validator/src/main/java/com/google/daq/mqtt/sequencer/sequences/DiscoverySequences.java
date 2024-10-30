13e405c788c2e71f5ed4bff85cca85204ca7b58b
Trevor <grafnu@users.noreply.github.com>
2024-10-30 12:34:52 -0700

    Fixing test logic for scan_single_future (#1007)

--- validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/DiscoverySequences.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/sequences/DiscoverySequences.java
@@ -81,7 +81,7 @@ public class DiscoverySequences extends SequenceBase {
   private static final long RANDOM_YEAR_SEC = (long) (Math.random() * 60 * 60 * 24 * 365);
   private static final Instant BASE_OLD_TIME = Instant.parse("2020-10-18T12:02:01Z");
   private static final Date LONG_TIME_AGO = Date.from(BASE_OLD_TIME.plusSeconds(RANDOM_YEAR_SEC));
-  private static final long SCAN_DURATION = 10;
+  private static final int SCAN_DURATION_SEC = 10;
   private Set<String> metaFamilies;
   private Instant scanGeneration;
 
@@ -288,13 +288,13 @@ public class DiscoverySequences extends SequenceBase {
     checkThat("scan started at time", deltaStart <= SCAN_START_JITTER_SEC,
         format("scan start %ss different from expected %s", deltaStart, isoConvert(expectedStart)));
 
-    Instant expectedFinish = expectedStart.plusSeconds(SCAN_DURATION);
+    Instant expectedFinish = scanStarted.plusSeconds(SCAN_DURATION_SEC);
     waitFor("scheduled scan complete", WAITING_PERIOD, this::detailScanStopped);
     Instant scanFinished = getNowInstant();
     long deltaFinish = Math.abs(Duration.between(scanFinished, expectedFinish).toSeconds());
     checkThat("scan completed at time", deltaFinish <= SCAN_START_JITTER_SEC,
         format("scan completed %ss different from expected %s", deltaFinish,
-            isoConvert(expectedStart)));
+            isoConvert(expectedFinish)));
 
     Integer stateEvents = deviceState.discovery.families.get(scanFamily).active_count;
     List<DiscoveryEvents> events = popReceivedEvents(DiscoveryEvents.class);
@@ -414,7 +414,7 @@ public class DiscoverySequences extends SequenceBase {
     configFamily.generation = SemanticDate.describe("family generation", startTime);
     configFamily.depth = enumerationDepthIf(shouldEnumerate);
     configFamily.scan_interval_sec = intervalSec;
-    configFamily.scan_duration_sec = ofNullable(intervalSec).orElse(SCAN_START_DELAY_SEC);
+    configFamily.scan_duration_sec = ofNullable(intervalSec).orElse(SCAN_DURATION_SEC);
     popReceivedEvents(DiscoveryEvents.class);
   }
 
