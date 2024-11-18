636ca737d9b1b0b72b64cb915beb40041c53f0c7
Noureddine El Saidi <elsaidi@google.com>
2024-11-18 12:40:29 +0000

    minor refator and read object list

--- misc/discoverynode/src/udmi/discovery/bacnet.py
+++ misc/discoverynode/src/udmi/discovery/bacnet.py
@@ -12,18 +12,24 @@ import time
 from typing import Any, Callable
 import xml.etree.ElementTree
 import BAC0
+import BAC0.core.io.IOExceptions
 import udmi.discovery.discovery as discovery
 import udmi.schema.discovery_event
 from udmi.schema.discovery_event import DiscoveryEvent
 import udmi.schema.state
+import ipaddress
 
 
 BAC0.log_level(log_file=None, stdout=None, stderr=None)
 BAC0.log_level("silence")
 
 
+class CowardlyQuit(Exception):
+  pass
+
+
 class GlobalBacnetDiscovery(discovery.DiscoveryController):
-  """Passive Network Discovery."""
+  """Bacnet discovery."""
 
   scan_family = "bacnet"
 
@@ -57,6 +63,59 @@ class GlobalBacnetDiscovery(discovery.DiscoveryController):
     self.cancelled = True
     self.result_producer_thread.join()
 
+  def discover_device(self,device) -> DiscoveryEvent:
+    (address, id) = device
+  
+    ### Existence of a device
+    ###################################################################
+    event = udmi.schema.discovery_event.DiscoveryEvent(
+        generation=self.config.generation,
+        scan_family=self.scan_family,
+        scan_addr=str(id),
+    )
+
+    try:
+      ipaddress.ip_address(address)
+    except ValueError:
+      pass
+    else:
+      event.families["ipv4"] = udmi.schema.discovery_event.DiscoveryFamily(
+          addr=address
+      ) 
+
+
+    ### Basic Properties
+    ###################################################################
+    try:
+      object_name, vendor_name, firmware_version, model_name, serial_number = (
+          self.bacnet.readMultiple(
+              f"{address} device {id} objectName vendorName"
+              " firmwareRevision modelName serialNumber"
+          )
+      )
+
+      logging.info("object_name: %s vendor_name: %s firmware: %s model: %s serial: %s",  object_name, vendor_name, firmware_version, model_name, serial_number)
+
+      event.system.serial_no = serial_number
+      event.system.hardware.make = vendor_name
+      event.system.hardware.model = model_name
+      event.system.software.firmware = firmware_version
+
+    except (BAC0.core.io.IOExceptions.SegmentationNotSupported, Exception) as err:
+      logging.exception(f"error reading from {address}/{id}")
+      return event
+
+    ### Points
+    ###################################################################
+
+    object_list = self.bacnet.readMultiple(
+        f"{address} device {id} obectList"
+    )
+
+    print(object_list)
+    logging.warning(object_list)
+    
+
   @discovery.catch_exceptions_to_state
   @discovery.main_task
   def result_producer(self):
@@ -68,35 +127,15 @@ class GlobalBacnetDiscovery(discovery.DiscoveryController):
               set(self.bacnet.discoveredDevices.keys()) - self.devices_published
           )
           for device in new_devices:
-            (address, id) = device
-
-            # if depths ...
-            # Get make and model
-            try:
-              object_name, vendor_name, firmware_version, model_name, serial_number = (
-                  self.bacnet.readMultiple(
-                      f"{address} device {id} objectName vendorName"
-                      " firmwareRevision modelName serialNumber"
-                  )
-              )
-            except ValueError:
-              logging.exception(f"error reading from {address}/{id}")
-              continue
+            # Check that it is not cancelled in the inner loop too because this
+            # can take a long time to enumerate through all found devices.
+            if self.cancelled:
+              break
             
-            logging.info("object_name %s vendor_name %s firmware %s model %s serial %s",  object_name, vendor_name, firmware_version, model_name, serial_number)
-            event = udmi.schema.discovery_event.DiscoveryEvent(
-                generation=self.config.generation,
-                scan_family=self.scan_family,
-                scan_addr=str(id),
-            )
-
-            event.families["ipv4"] = udmi.schema.discovery_event.DiscoveryFamily(
-                addr=address
-            ) 
-            event.system.serial_no = serial_number
-            event.system.hardware.make = vendor_name
-            event.system.hardware.model = model_name
-            event.system.software.firmware = firmware_version
+            start = time.monotonic()
+            event = self.discover_device(device)
+            end = time.monotonic() 
+            logging.info(f"discovery for {device} in {end - start} seconds")
 
             self.publish(event)
             self.devices_published.add(device)
