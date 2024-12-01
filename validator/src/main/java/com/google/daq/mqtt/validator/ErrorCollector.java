b7527ebdb96a60394bace3889ef8fb0525ec158f
Trevor Pering <peringknife@google.com>
2024-12-01 08:50:07 -0800

    One more javadoc

--- validator/src/main/java/com/google/daq/mqtt/validator/ErrorCollector.java
+++ validator/src/main/java/com/google/daq/mqtt/validator/ErrorCollector.java
@@ -1,5 +1,8 @@
 package com.google.daq.mqtt.validator;
 
+/**
+ * Simple interface for collecting errors of various kinds.
+ */
 public interface ErrorCollector {
 
   void addError(Exception error, String category, String detail);
