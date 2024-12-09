f0e8faddf9dfebf0cf31d7269707353ef3d70146
Mert Cingöz <mertcingoz@gmail.com>
2024-12-09 11:16:05 +0100

    Add gateway.status as allowed system state change

--- validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
+++ validator/src/main/java/com/google/daq/mqtt/sequencer/SequenceBase.java
@@ -251,8 +251,8 @@ public class SequenceBase {
   private static final Duration LOG_WAIT_TIME = Duration.ofSeconds(30);
   private static final Duration DEFAULT_LOOP_TIMEOUT = Duration.ofHours(30);
   private static final Set<String> SYSTEM_STATE_CHANGES = ImmutableSet.of(
-      "timestamp", "system.last_config", "system.status");
   private static final long EVENT_WAIT_DELAY_MS = 1000;
+      "timestamp", "system.last_config", "system.status", "gateway.status");
   private static final Duration STATE_TIMESTAMP_ERROR_THRESHOLD = Duration.ofMinutes(20);
   private static final Set<IotAccess.IotProvider> SEQUENCER_PROVIDERS = ImmutableSet.of(
       IotProvider.GBOS, IotProvider.MQTT, IotProvider.GREF);
