4d1c77a34063f273fff67cac253f5650dac77aba
Trevor Pering <peringknife@google.com>
2025-01-13 10:57:56 -0800

    Adding gateway stuff to cloud model

--- gencode/java/udmi/schema/CloudModel.java
+++ gencode/java/udmi/schema/CloudModel.java
@@ -28,6 +28,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
     "auth_type",
     "device_key",
     "resource_type",
+    "gateway",
     "config",
     "blocked",
     "detail",
@@ -88,6 +89,15 @@ public class CloudModel {
     @JsonProperty("resource_type")
     @JsonPropertyDescription("The resource type of this model")
     public CloudModel.Resource_type resource_type;
+    /**
+     * Gateway Model
+     * <p>
+     * [Gateway Documentation](../docs/specs/gateway.md)
+     * 
+     */
+    @JsonProperty("gateway")
+    @JsonPropertyDescription("[Gateway Documentation](../docs/specs/gateway.md)")
+    public GatewayModel gateway;
     /**
      * Cloud Config Model
      * <p>
@@ -159,6 +169,7 @@ public class CloudModel {
         result = ((result* 31)+((this.config == null)? 0 :this.config.hashCode()));
         result = ((result* 31)+((this.last_config_ack == null)? 0 :this.last_config_ack.hashCode()));
         result = ((result* 31)+((this.operation == null)? 0 :this.operation.hashCode()));
+        result = ((result* 31)+((this.gateway == null)? 0 :this.gateway.hashCode()));
         result = ((result* 31)+((this.timestamp == null)? 0 :this.timestamp.hashCode()));
         return result;
     }
@@ -172,7 +183,7 @@ public class CloudModel {
             return false;
         }
         CloudModel rhs = ((CloudModel) other);
-        return ((((((((((((((((((((((this.updated_time == rhs.updated_time)||((this.updated_time!= null)&&this.updated_time.equals(rhs.updated_time)))&&((this.auth_type == rhs.auth_type)||((this.auth_type!= null)&&this.auth_type.equals(rhs.auth_type))))&&((this.device_key == rhs.device_key)||((this.device_key!= null)&&this.device_key.equals(rhs.device_key))))&&((this.metadata == rhs.metadata)||((this.metadata!= null)&&this.metadata.equals(rhs.metadata))))&&((this.connection_type == rhs.connection_type)||((this.connection_type!= null)&&this.connection_type.equals(rhs.connection_type))))&&((this.last_event_time == rhs.last_event_time)||((this.last_event_time!= null)&&this.last_event_time.equals(rhs.last_event_time))))&&((this.last_config_time == rhs.last_config_time)||((this.last_config_time!= null)&&this.last_config_time.equals(rhs.last_config_time))))&&((this.credentials == rhs.credentials)||((this.credentials!= null)&&this.credentials.equals(rhs.credentials))))&&((this.last_state_time == rhs.last_state_time)||((this.last_state_time!= null)&&this.last_state_time.equals(rhs.last_state_time))))&&((this.resource_type == rhs.resource_type)||((this.resource_type!= null)&&this.resource_type.equals(rhs.resource_type))))&&((this.num_id == rhs.num_id)||((this.num_id!= null)&&this.num_id.equals(rhs.num_id))))&&((this.version == rhs.version)||((this.version!= null)&&this.version.equals(rhs.version))))&&((this.blocked == rhs.blocked)||((this.blocked!= null)&&this.blocked.equals(rhs.blocked))))&&((this.last_error_time == rhs.last_error_time)||((this.last_error_time!= null)&&this.last_error_time.equals(rhs.last_error_time))))&&((this.metadata_str == rhs.metadata_str)||((this.metadata_str!= null)&&this.metadata_str.equals(rhs.metadata_str))))&&((this.detail == rhs.detail)||((this.detail!= null)&&this.detail.equals(rhs.detail))))&&((this.device_ids == rhs.device_ids)||((this.device_ids!= null)&&this.device_ids.equals(rhs.device_ids))))&&((this.config == rhs.config)||((this.config!= null)&&this.config.equals(rhs.config))))&&((this.last_config_ack == rhs.last_config_ack)||((this.last_config_ack!= null)&&this.last_config_ack.equals(rhs.last_config_ack))))&&((this.operation == rhs.operation)||((this.operation!= null)&&this.operation.equals(rhs.operation))))&&((this.timestamp == rhs.timestamp)||((this.timestamp!= null)&&this.timestamp.equals(rhs.timestamp))));
+        return (((((((((((((((((((((((this.updated_time == rhs.updated_time)||((this.updated_time!= null)&&this.updated_time.equals(rhs.updated_time)))&&((this.auth_type == rhs.auth_type)||((this.auth_type!= null)&&this.auth_type.equals(rhs.auth_type))))&&((this.device_key == rhs.device_key)||((this.device_key!= null)&&this.device_key.equals(rhs.device_key))))&&((this.metadata == rhs.metadata)||((this.metadata!= null)&&this.metadata.equals(rhs.metadata))))&&((this.connection_type == rhs.connection_type)||((this.connection_type!= null)&&this.connection_type.equals(rhs.connection_type))))&&((this.last_event_time == rhs.last_event_time)||((this.last_event_time!= null)&&this.last_event_time.equals(rhs.last_event_time))))&&((this.last_config_time == rhs.last_config_time)||((this.last_config_time!= null)&&this.last_config_time.equals(rhs.last_config_time))))&&((this.credentials == rhs.credentials)||((this.credentials!= null)&&this.credentials.equals(rhs.credentials))))&&((this.last_state_time == rhs.last_state_time)||((this.last_state_time!= null)&&this.last_state_time.equals(rhs.last_state_time))))&&((this.resource_type == rhs.resource_type)||((this.resource_type!= null)&&this.resource_type.equals(rhs.resource_type))))&&((this.num_id == rhs.num_id)||((this.num_id!= null)&&this.num_id.equals(rhs.num_id))))&&((this.version == rhs.version)||((this.version!= null)&&this.version.equals(rhs.version))))&&((this.blocked == rhs.blocked)||((this.blocked!= null)&&this.blocked.equals(rhs.blocked))))&&((this.last_error_time == rhs.last_error_time)||((this.last_error_time!= null)&&this.last_error_time.equals(rhs.last_error_time))))&&((this.metadata_str == rhs.metadata_str)||((this.metadata_str!= null)&&this.metadata_str.equals(rhs.metadata_str))))&&((this.detail == rhs.detail)||((this.detail!= null)&&this.detail.equals(rhs.detail))))&&((this.device_ids == rhs.device_ids)||((this.device_ids!= null)&&this.device_ids.equals(rhs.device_ids))))&&((this.config == rhs.config)||((this.config!= null)&&this.config.equals(rhs.config))))&&((this.last_config_ack == rhs.last_config_ack)||((this.last_config_ack!= null)&&this.last_config_ack.equals(rhs.last_config_ack))))&&((this.operation == rhs.operation)||((this.operation!= null)&&this.operation.equals(rhs.operation))))&&((this.gateway == rhs.gateway)||((this.gateway!= null)&&this.gateway.equals(rhs.gateway))))&&((this.timestamp == rhs.timestamp)||((this.timestamp!= null)&&this.timestamp.equals(rhs.timestamp))));
     }
 
 
--- gencode/python/udmi/schema/model_cloud.py
+++ gencode/python/udmi/schema/model_cloud.py
@@ -1,4 +1,5 @@
 """Generated class for model_cloud.json"""
+from .model_gateway import GatewayModel
 from .model_cloud_config import CloudConfigModel
 
 
@@ -46,6 +47,7 @@ class CloudModel:
     self.auth_type = None
     self.device_key = None
     self.resource_type = None
+    self.gateway = None
     self.config = None
     self.blocked = None
     self.detail = None
@@ -73,6 +75,7 @@ class CloudModel:
     result.auth_type = source.get('auth_type')
     result.device_key = source.get('device_key')
     result.resource_type = source.get('resource_type')
+    result.gateway = GatewayModel.from_dict(source.get('gateway'))
     result.config = CloudConfigModel.from_dict(source.get('config'))
     result.blocked = source.get('blocked')
     result.detail = source.get('detail')
@@ -120,6 +123,8 @@ class CloudModel:
       result['device_key'] = self.device_key # 5
     if self.resource_type:
       result['resource_type'] = self.resource_type # 5
+    if self.gateway:
+      result['gateway'] = self.gateway.to_dict() # 4
     if self.config:
       result['config'] = self.config.to_dict() # 4
     if self.blocked:
