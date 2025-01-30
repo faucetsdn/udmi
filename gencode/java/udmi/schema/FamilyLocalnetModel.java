6e2a95d7114431e6c5718fa29cae29d8432edfb7
Trevor Pering <peringknife@google.com>
2025-01-30 15:37:53 -0800

    Adding localnet properties

--- gencode/java/udmi/schema/FamilyLocalnetModel.java
+++ gencode/java/udmi/schema/FamilyLocalnetModel.java
@@ -1,6 +1,7 @@
 
 package udmi.schema;
 
+import java.util.HashMap;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
@@ -16,6 +17,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "addr",
+    "network",
+    "parent_id",
     "family"
 })
 public class FamilyLocalnetModel {
@@ -26,15 +29,26 @@ public class FamilyLocalnetModel {
      */
     @JsonProperty("addr")
     @JsonPropertyDescription("The address of a device on the fieldbus/local network")
-    public String addr;
+    public java.lang.String addr;
+    @JsonProperty("network")
+    public HashMap<String, String> network;
+    /**
+     * The device id of the network parent
+     * 
+     */
+    @JsonProperty("parent_id")
+    @JsonPropertyDescription("The device id of the network parent")
+    public java.lang.String parent_id;
     @JsonProperty("family")
-    public String family;
+    public java.lang.String family;
 
     @Override
     public int hashCode() {
         int result = 1;
         result = ((result* 31)+((this.addr == null)? 0 :this.addr.hashCode()));
         result = ((result* 31)+((this.family == null)? 0 :this.family.hashCode()));
+        result = ((result* 31)+((this.parent_id == null)? 0 :this.parent_id.hashCode()));
+        result = ((result* 31)+((this.network == null)? 0 :this.network.hashCode()));
         return result;
     }
 
@@ -47,7 +61,7 @@ public class FamilyLocalnetModel {
             return false;
         }
         FamilyLocalnetModel rhs = ((FamilyLocalnetModel) other);
-        return (((this.addr == rhs.addr)||((this.addr!= null)&&this.addr.equals(rhs.addr)))&&((this.family == rhs.family)||((this.family!= null)&&this.family.equals(rhs.family))));
+        return (((((this.addr == rhs.addr)||((this.addr!= null)&&this.addr.equals(rhs.addr)))&&((this.family == rhs.family)||((this.family!= null)&&this.family.equals(rhs.family))))&&((this.parent_id == rhs.parent_id)||((this.parent_id!= null)&&this.parent_id.equals(rhs.parent_id))))&&((this.network == rhs.network)||((this.network!= null)&&this.network.equals(rhs.network))));
     }
 
 }
--- gencode/python/udmi/schema/model_localnet_family.py
+++ gencode/python/udmi/schema/model_localnet_family.py
@@ -1,11 +1,47 @@
 """Generated class for model_localnet_family.json"""
 
 
+class Object9FFEC3F0:
+  """Generated schema class"""
+
+  def __init__(self):
+    pass
+
+  @staticmethod
+  def from_dict(source):
+    if not source:
+      return None
+    result = Object9FFEC3F0()
+    return result
+
+  @staticmethod
+  def map_from(source):
+    if not source:
+      return None
+    result = {}
+    for key in source:
+      result[key] = Object9FFEC3F0.from_dict(source[key])
+    return result
+
+  @staticmethod
+  def expand_dict(input):
+    result = {}
+    for property in input:
+      result[property] = input[property].to_dict() if input[property] else {}
+    return result
+
+  def to_dict(self):
+    result = {}
+    return result
+
+
 class FamilyLocalnetModel:
   """Generated schema class"""
 
   def __init__(self):
     self.addr = None
+    self.network = None
+    self.parent_id = None
     self.family = None
 
   @staticmethod
@@ -14,6 +50,8 @@ class FamilyLocalnetModel:
       return None
     result = FamilyLocalnetModel()
     result.addr = source.get('addr')
+    result.network = Object9FFEC3F0.from_dict(source.get('network'))
+    result.parent_id = source.get('parent_id')
     result.family = source.get('family')
     return result
 
@@ -37,6 +75,10 @@ class FamilyLocalnetModel:
     result = {}
     if self.addr:
       result['addr'] = self.addr # 5
+    if self.network:
+      result['network'] = self.network.to_dict() # 4
+    if self.parent_id:
+      result['parent_id'] = self.parent_id # 5
     if self.family:
       result['family'] = self.family # 5
     return result
