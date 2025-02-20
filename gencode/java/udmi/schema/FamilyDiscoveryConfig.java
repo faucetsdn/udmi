37faf3e96a01f2425b9d94f3c7f5c4ec22bf260c
Trevor Pering <peringknife@google.com>
2025-02-20 05:01:17 -0800

    GEncode and upjava

--- gencode/java/udmi/schema/FamilyDiscoveryConfig.java
+++ gencode/java/udmi/schema/FamilyDiscoveryConfig.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
     "generation",
     "scan_interval_sec",
     "scan_duration_sec",
+    "addrs",
     "passive_sec",
     "depth"
 })
@@ -45,6 +46,8 @@ public class FamilyDiscoveryConfig {
     @JsonProperty("scan_duration_sec")
     @JsonPropertyDescription("Scan duration, in seconds")
     public Integer scan_duration_sec;
+    @JsonProperty("addrs")
+    public Object addrs;
     /**
      * Holdoff time for passively discovered devices
      * 
@@ -63,6 +66,7 @@ public class FamilyDiscoveryConfig {
         result = ((result* 31)+((this.depth == null)? 0 :this.depth.hashCode()));
         result = ((result* 31)+((this.passive_sec == null)? 0 :this.passive_sec.hashCode()));
         result = ((result* 31)+((this.scan_duration_sec == null)? 0 :this.scan_duration_sec.hashCode()));
+        result = ((result* 31)+((this.addrs == null)? 0 :this.addrs.hashCode()));
         return result;
     }
 
@@ -75,7 +79,7 @@ public class FamilyDiscoveryConfig {
             return false;
         }
         FamilyDiscoveryConfig rhs = ((FamilyDiscoveryConfig) other);
-        return ((((((this.generation == rhs.generation)||((this.generation!= null)&&this.generation.equals(rhs.generation)))&&((this.scan_interval_sec == rhs.scan_interval_sec)||((this.scan_interval_sec!= null)&&this.scan_interval_sec.equals(rhs.scan_interval_sec))))&&((this.depth == rhs.depth)||((this.depth!= null)&&this.depth.equals(rhs.depth))))&&((this.passive_sec == rhs.passive_sec)||((this.passive_sec!= null)&&this.passive_sec.equals(rhs.passive_sec))))&&((this.scan_duration_sec == rhs.scan_duration_sec)||((this.scan_duration_sec!= null)&&this.scan_duration_sec.equals(rhs.scan_duration_sec))));
+        return (((((((this.generation == rhs.generation)||((this.generation!= null)&&this.generation.equals(rhs.generation)))&&((this.scan_interval_sec == rhs.scan_interval_sec)||((this.scan_interval_sec!= null)&&this.scan_interval_sec.equals(rhs.scan_interval_sec))))&&((this.depth == rhs.depth)||((this.depth!= null)&&this.depth.equals(rhs.depth))))&&((this.passive_sec == rhs.passive_sec)||((this.passive_sec!= null)&&this.passive_sec.equals(rhs.passive_sec))))&&((this.scan_duration_sec == rhs.scan_duration_sec)||((this.scan_duration_sec!= null)&&this.scan_duration_sec.equals(rhs.scan_duration_sec))))&&((this.addrs == rhs.addrs)||((this.addrs!= null)&&this.addrs.equals(rhs.addrs))));
     }
 
 }
