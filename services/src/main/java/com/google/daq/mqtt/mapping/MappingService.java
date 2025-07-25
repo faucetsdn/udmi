92be8ee738d204cdb7f569a668f6e2979ff54623
Mapping backend runner jainnitish~ <insecure-cloudtop-shared-user@cloudtop-prod-asia-southeast.iam.gserviceaccount.com>
2025-07-25 09:19:51 +0000

    initiate mapping process on consuming discovery complete triggger

--- services/src/main/java/com/google/daq/mqtt/mapping/MappingService.java
+++ services/src/main/java/com/google/daq/mqtt/mapping/MappingService.java
@@ -1,102 +1,126 @@
 package com.google.daq.mqtt.mapping;
 
+import static com.google.bos.iot.core.reconcile.SourceRepoMessageUtils.parseSourceRepoMessageData;
+import static com.google.udmi.util.GeneralUtils.isNotEmpty;
 
 import com.google.daq.mqtt.registrar.Registrar;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.udmi.util.AbstractPollingService;
 import com.google.udmi.util.SourceRepository;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Mapping Service: which currently runs from command line
+ * Mapping Service basically
  * fetches the site mode, internally calls registrar, then the mapping process
- * and then pushes the changes in the different proposal branch.
+ * and then pushes the changes in the different discovery branch.
  */
-public class MappingService {
+public class MappingService extends AbstractPollingService {
   private static final Logger LOGGER = LoggerFactory.getLogger(MappingService.class);
   private static final String SERVICE_NAME = "MappingService";
-  private SourceRepository sourceRepository;
-  private final int requiredArgumentLength = 6;
-  private final String registryId;
-  private final String projectId;
-  private final String baseCloningDirectory;
+  private static final String SUBSCRIPTION_SUFFIX = "udmi_target_mapping_updates";
+  private static final String EVENT_NUMBER_FIELD = "event_no";
+  private static final String FAMILY_FIELD = "family";
+  private static final String REGISTRY_ID_FIELD = "deviceRegistryId";
+  private static final String DISCOVERY_NODE_DEVICE_ID_FIELD = "deviceId";
+  private static final String TRIGGER_BRANCH = "discovery";
+  private static final String DEFAULT_TARGET_BRANCH = "main";
   private final String projectSpec;
-  private final String mappingFamily;
-  private final String discoveryNodeDeviceId;
-  private static final String IMPORT_BRANCH = "main";
 
   /**
-   * Mapping Service for mapping the discovery results
-   * accepts commandlineArguments as
-   * registryId projectId baseCloningDirectory projectSpec mappingFamily discoveryNodeDeviceId
-   * registryId: e.g. ZZ-TRI-FECTA
-   * projectId: GCP project id
-   * baseCloningDirectory: local path for the repository
-   * projectSpec: eg. //mqtt/localhost
-   * mappingFamily:vendor/bacnet
-   * discoveryNodeDeviceId: discoveryNode deviceId e.g. GAT-123
+   * Main entry point for the application.
    */
   public static void main(String[] args) {
-    MappingService mappingService = new MappingService(args);
-    mappingService.initialize();
+    if (args.length < 3 || args.length > 4) {
+      System.err.println(
+          "Usage: MappingService <projectTarget> <registrarTarget> <siteModelCloneDir> "
+              + "[<localOriginDir>]");
+      System.exit(1);
+    }
 
-    mappingService.cloneRepo();
-    mappingService.process();
+    String projectTarget = args[0];
+    String projectSpec = args[1];
+    String siteModelCloneDir = args[2];
+    String localOriginDir =  (args.length == 4) && isNotEmpty(args[3]) ? args[3] : null;
+
+    MappingService service = new MappingService(projectTarget, projectSpec, siteModelCloneDir,
+        localOriginDir);
+    service.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      LOGGER.info("Shutdown hook triggered for {}", SERVICE_NAME);
+      service.stop();
+    }));
   }
 
   /**
-   * Initializes new Mapping Service.
+   * Primary constructor for the MappingService.
    *
-   * @param args commandline args as:site_path project_spec
+   * @param projectTarget Target project specifier, e.g., "//pubsub/gcp-project/udmi-namespace"
+   * @param siteModelBaseDir Base directory for cloning site model Git repositories.
+   * @param localOriginDir Optional directory for local git origins (for testing).
    */
-  public MappingService(String[] args) {
-    if (args.length != requiredArgumentLength) {
-      throw new IllegalArgumentException("Invalid arguments provided");
-    }
-
-    this.registryId = args[0];
-    this.projectId = args[1];
-    this.baseCloningDirectory = args[2];
-    this.projectSpec = args[3];
-    this.mappingFamily = args[4];
-    this.discoveryNodeDeviceId = args[5];
+  public MappingService(String projectTarget, String projectSpec, String siteModelBaseDir, String localOriginDir) {
+    super(SERVICE_NAME, SUBSCRIPTION_SUFFIX, projectTarget, siteModelBaseDir, localOriginDir);
+    this.projectSpec = projectSpec;
+    LOGGER.info("Starting Mapping Service for project {}, cloning to {}", projectTarget,
+        siteModelBaseDir);
   }
 
-  private void cloneRepo() {
-    if (!sourceRepository.clone(IMPORT_BRANCH)) {
-      throw new RuntimeException("Could not clone repository " + registryId);
+  @Override
+  protected void handleMessage(PubsubMessage message) {
+    try {
+      Map<String, Object> messageData = parseSourceRepoMessageData(message);
+      Integer eventNumber = (Integer) messageData.getOrDefault(EVENT_NUMBER_FIELD, 0);
+      LOGGER.info("Received event no. from message: {}", eventNumber);
+      if (eventNumber < 0) {
+        String mappingFamily = (String) messageData.getOrDefault(FAMILY_FIELD, "");
+        String registryId = message.getAttributesOrDefault(REGISTRY_ID_FIELD, "");
+
+        if (registryId.isEmpty()) {
+          LOGGER.error("Registry Id not found for the message.");
+          return;
+        }
+
+        String discoveryNodeDeviceId = message.getAttributesOrDefault(DISCOVERY_NODE_DEVICE_ID_FIELD, "");
+        if (discoveryNodeDeviceId.isEmpty()) {
+          LOGGER.error("Discovery Node device Id not found for the message received.");
+        }
+        LOGGER.info("Starting Mapping process for registry: {}, family: {}, discoverNode deviceId: {}", registryId, mappingFamily, discoveryNodeDeviceId);
+
+        SourceRepository repository = initRepository(registryId);
+        if (repository.clone(DEFAULT_TARGET_BRANCH)) {
+          String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmmss"));
+
+          String exportBranch = String.format("%s/%s/%s", TRIGGER_BRANCH, SERVICE_NAME, timestamp);
+          if (!repository.checkoutNewBranch(exportBranch)) {
+            throw new RuntimeException("Unable to create and checkout export branch " + exportBranch);
+          }
+
+          String udmiModelPath = repository.getUdmiModelPath();
+          (new Registrar()).processArgs(new ArrayList<>(List.of(udmiModelPath, projectSpec))).execute();
+          MappingAgent mappingAgent = new MappingAgent(new ArrayList<>(
+              List.of(udmiModelPath, projectSpec)));
+          mappingAgent.processMapping(new ArrayList<>(List.of(discoveryNodeDeviceId, mappingFamily)));
+
+          LOGGER.info("Committing and pushing changes to branch {}", exportBranch);
+          if (!repository.commitAndPush("Merge changes from source: MappingService")) {
+            throw new RuntimeException("Unable to commit and push changes to branch " + exportBranch);
+          }
+          LOGGER.info("Export operation complete.");
+          repository.delete();
+        } else {
+          LOGGER.error("Could not clone repository! PR message was not published!");
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException("Could not process Pub/Sub message", e);
     }
   }
-
-  private void initialize() {
-    String repoCloneDir = Paths.get(baseCloningDirectory, registryId).toString();
-    sourceRepository = new SourceRepository(registryId, repoCloneDir, null, projectId, null);
-
-  }
-
-  private void process() {
-    String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmmss"));
-
-    String exportBranch = String.format("proposal/%s/%s", SERVICE_NAME, timestamp);
-    if (!sourceRepository.checkoutNewBranch(exportBranch)) {
-      throw new RuntimeException("Unable to create and checkout export branch " + exportBranch);
-    }
-
-    String udmiModelPath = sourceRepository.getUdmiModelPath();
-    (new Registrar()).processArgs(new ArrayList<>(List.of(udmiModelPath, projectSpec))).execute();
-    MappingAgent mappingAgent = new MappingAgent(new ArrayList<>(
-        List.of(udmiModelPath, projectSpec)));
-    mappingAgent.processMapping(new ArrayList<>(List.of(discoveryNodeDeviceId, mappingFamily)));
-
-    LOGGER.info("Committing and pushing changes to branch {}", exportBranch);
-    if (!sourceRepository.commitAndPush("Merge changes from source: MappingService")) {
-      throw new RuntimeException("Unable to commit and push changes to branch " + exportBranch);
-    }
-    LOGGER.info("Export operation complete.");
-  }
-
 }
