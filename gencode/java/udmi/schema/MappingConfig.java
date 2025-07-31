5640173873398ae8a2bdb372bf4f61b446009036
Nitish Jain <jainrocks@users.noreply.github.com>
2025-07-31 10:42:44 +0530

    changes for garbage collection of stale entries (#1165)

--- gencode/java/udmi/schema/MappingConfig.java
+++ gencode/java/udmi/schema/MappingConfig.java
@@ -19,7 +19,9 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonPropertyOrder({
     "timestamp",
     "version",
-    "devices"
+    "devices",
+    "extras_deletion_days",
+    "devices_deletion_days"
 })
 public class MappingConfig {
 
@@ -44,10 +46,26 @@ public class MappingConfig {
     @JsonProperty("devices")
     @JsonPropertyDescription("Configuration of mapped devices")
     public HashMap<String, DeviceMappingConfig> devices;
+    /**
+     * extras discovery event garbage collection time
+     * 
+     */
+    @JsonProperty("extras_deletion_days")
+    @JsonPropertyDescription("extras discovery event garbage collection time")
+    public Integer extras_deletion_days;
+    /**
+     * devices garbage collection time
+     * 
+     */
+    @JsonProperty("devices_deletion_days")
+    @JsonPropertyDescription("devices garbage collection time")
+    public Integer devices_deletion_days;
 
     @Override
     public int hashCode() {
         int result = 1;
+        result = ((result* 31)+((this.extras_deletion_days == null)? 0 :this.extras_deletion_days.hashCode()));
+        result = ((result* 31)+((this.devices_deletion_days == null)? 0 :this.devices_deletion_days.hashCode()));
         result = ((result* 31)+((this.version == null)? 0 :this.version.hashCode()));
         result = ((result* 31)+((this.devices == null)? 0 :this.devices.hashCode()));
         result = ((result* 31)+((this.timestamp == null)? 0 :this.timestamp.hashCode()));
@@ -63,7 +81,7 @@ public class MappingConfig {
             return false;
         }
         MappingConfig rhs = ((MappingConfig) other);
-        return ((((this.version == rhs.version)||((this.version!= null)&&this.version.equals(rhs.version)))&&((this.devices == rhs.devices)||((this.devices!= null)&&this.devices.equals(rhs.devices))))&&((this.timestamp == rhs.timestamp)||((this.timestamp!= null)&&this.timestamp.equals(rhs.timestamp))));
+        return ((((((this.extras_deletion_days == rhs.extras_deletion_days)||((this.extras_deletion_days!= null)&&this.extras_deletion_days.equals(rhs.extras_deletion_days)))&&((this.devices_deletion_days == rhs.devices_deletion_days)||((this.devices_deletion_days!= null)&&this.devices_deletion_days.equals(rhs.devices_deletion_days))))&&((this.version == rhs.version)||((this.version!= null)&&this.version.equals(rhs.version))))&&((this.devices == rhs.devices)||((this.devices!= null)&&this.devices.equals(rhs.devices))))&&((this.timestamp == rhs.timestamp)||((this.timestamp!= null)&&this.timestamp.equals(rhs.timestamp))));
     }
 
 }
