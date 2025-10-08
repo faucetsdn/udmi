7fe17cb56f0fa2d13f7a2d8f41ecda31ba6dd1f1
Khyati Mahendru <heykhyati@google.com>
2025-10-08 10:46:15 +0000

    Remove redundant schema for model_points_template

--- gencode/java/udmi/schema/PointsetModel.java
+++ gencode/java/udmi/schema/PointsetModel.java
@@ -17,7 +17,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "points",
-    "points_template",
     "exclude_units_from_config",
     "exclude_points_from_config",
     "sample_limit_sec",
@@ -33,15 +32,6 @@ public class PointsetModel {
     @JsonProperty("points")
     @JsonPropertyDescription("Information about a specific point name of the device.")
     public HashMap<String, PointPointsetModel> points;
-    /**
-     * Points Template Model
-     * <p>
-     * Points template representing a collection of points which can be reused by multiple devices
-     * 
-     */
-    @JsonProperty("points_template")
-    @JsonPropertyDescription("Points template representing a collection of points which can be reused by multiple devices")
-    public PointsTemplateModel points_template;
     @JsonProperty("exclude_units_from_config")
     public Boolean exclude_units_from_config;
     @JsonProperty("exclude_points_from_config")
@@ -66,7 +56,6 @@ public class PointsetModel {
         int result = 1;
         result = ((result* 31)+((this.sample_rate_sec == null)? 0 :this.sample_rate_sec.hashCode()));
         result = ((result* 31)+((this.exclude_units_from_config == null)? 0 :this.exclude_units_from_config.hashCode()));
-        result = ((result* 31)+((this.points_template == null)? 0 :this.points_template.hashCode()));
         result = ((result* 31)+((this.sample_limit_sec == null)? 0 :this.sample_limit_sec.hashCode()));
         result = ((result* 31)+((this.points == null)? 0 :this.points.hashCode()));
         result = ((result* 31)+((this.exclude_points_from_config == null)? 0 :this.exclude_points_from_config.hashCode()));
@@ -82,7 +71,7 @@ public class PointsetModel {
             return false;
         }
         PointsetModel rhs = ((PointsetModel) other);
-        return (((((((this.sample_rate_sec == rhs.sample_rate_sec)||((this.sample_rate_sec!= null)&&this.sample_rate_sec.equals(rhs.sample_rate_sec)))&&((this.exclude_units_from_config == rhs.exclude_units_from_config)||((this.exclude_units_from_config!= null)&&this.exclude_units_from_config.equals(rhs.exclude_units_from_config))))&&((this.points_template == rhs.points_template)||((this.points_template!= null)&&this.points_template.equals(rhs.points_template))))&&((this.sample_limit_sec == rhs.sample_limit_sec)||((this.sample_limit_sec!= null)&&this.sample_limit_sec.equals(rhs.sample_limit_sec))))&&((this.points == rhs.points)||((this.points!= null)&&this.points.equals(rhs.points))))&&((this.exclude_points_from_config == rhs.exclude_points_from_config)||((this.exclude_points_from_config!= null)&&this.exclude_points_from_config.equals(rhs.exclude_points_from_config))));
+        return ((((((this.sample_rate_sec == rhs.sample_rate_sec)||((this.sample_rate_sec!= null)&&this.sample_rate_sec.equals(rhs.sample_rate_sec)))&&((this.exclude_units_from_config == rhs.exclude_units_from_config)||((this.exclude_units_from_config!= null)&&this.exclude_units_from_config.equals(rhs.exclude_units_from_config))))&&((this.sample_limit_sec == rhs.sample_limit_sec)||((this.sample_limit_sec!= null)&&this.sample_limit_sec.equals(rhs.sample_limit_sec))))&&((this.points == rhs.points)||((this.points!= null)&&this.points.equals(rhs.points))))&&((this.exclude_points_from_config == rhs.exclude_points_from_config)||((this.exclude_points_from_config!= null)&&this.exclude_points_from_config.equals(rhs.exclude_points_from_config))));
     }
 
 }
