cb845280d55dc0740f7373a2a93b826f05adf703
Khyati Mahendru <heykhyati@google.com>
2025-11-04 09:28:59 +0000

    Use absolute imports

--- clientlib/python/src/udmi/core/__init__.py
+++ clientlib/python/src/udmi/core/__init__.py
@@ -5,7 +5,7 @@ This package brings key classes and factory functions to the top-level
 `udmi.core` namespace for convenient access.
 """
 
-from .device import Device
-from .factory import create_mqtt_device_instance
-from .factory import create_device_with_basic_auth
-from .factory import create_device_with_jwt
+from udmi.core.device import Device
+from udmi.core.factory import create_device_with_basic_auth
+from udmi.core.factory import create_device_with_jwt
+from udmi.core.factory import create_mqtt_device_instance
