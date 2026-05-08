d5c982d7bea146911b2c7557f508fb93cd0bda88
Noureddine <noureddine@saidi.uk>
2026-05-08 18:27:28 +0000

    gencode

--- gencode/java/udmi/schema/FamilyLocalnetModel.java
+++ gencode/java/udmi/schema/FamilyLocalnetModel.java
@@ -74,6 +74,7 @@ public class FamilyLocalnetModel {
     @Override
     public int hashCode() {
         int result = 1;
+        result = ((result* 31)+((this.network_id == null)? 0 :this.network_id.hashCode()));
         result = ((result* 31)+((this.shadow_id == null)? 0 :this.shadow_id.hashCode()));
         result = ((result* 31)+((this.parent_id == null)? 0 :this.parent_id.hashCode()));
         result = ((result* 31)+((this.addr == null)? 0 :this.addr.hashCode()));
@@ -92,7 +93,7 @@ public class FamilyLocalnetModel {
             return false;
         }
         FamilyLocalnetModel rhs = ((FamilyLocalnetModel) other);
-        return (((((((this.addr == rhs.addr)||((this.addr!= null)&&this.addr.equals(rhs.addr)))&&((this.adjunct == rhs.adjunct)||((this.adjunct!= null)&&this.adjunct.equals(rhs.adjunct))))&&((this.family == rhs.family)||((this.family!= null)&&this.family.equals(rhs.family))))&&((this.shadow_id == rhs.shadow_id)||((this.shadow_id!= null)&&this.shadow_id.equals(rhs.shadow_id))))&&((this.parent_id == rhs.parent_id)||((this.parent_id!= null)&&this.parent_id.equals(rhs.parent_id))))&&((this.network == rhs.network)||((this.network!= null)&&this.network.equals(rhs.network))));
+        return ((((((((this.network_id == rhs.network_id)||((this.network_id!= null)&&this.network_id.equals(rhs.network_id)))&&((this.shadow_id == rhs.shadow_id)||((this.shadow_id!= null)&&this.shadow_id.equals(rhs.shadow_id))))&&((this.parent_id == rhs.parent_id)||((this.parent_id!= null)&&this.parent_id.equals(rhs.parent_id))))&&((this.addr == rhs.addr)||((this.addr!= null)&&this.addr.equals(rhs.addr))))&&((this.adjunct == rhs.adjunct)||((this.adjunct!= null)&&this.adjunct.equals(rhs.adjunct))))&&((this.family == rhs.family)||((this.family!= null)&&this.family.equals(rhs.family))))&&((this.network == rhs.network)||((this.network!= null)&&this.network.equals(rhs.network))));
     }
 
 }
