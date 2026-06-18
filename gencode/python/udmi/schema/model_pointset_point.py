7c664b80bf25046fee4d9971509f016f200677b5
Trevor <grafnu@users.noreply.github.com>
2026-06-18 07:14:24 -0700

    Updating to "expr" for calculated points (#1320)

--- gencode/python/udmi/schema/model_pointset_point.py
+++ gencode/python/udmi/schema/model_pointset_point.py
@@ -8,6 +8,7 @@ from enum import Enum
 from typing import Any, Dict, List, Optional
 
 from ._base import DataModel
+from .building_translation import BuildingTranslation
 from .discovery_ref import RefDiscovery
 
 
@@ -43,6 +44,7 @@ class PointPointsetModel(DataModel):
     """
 
     units: Optional[str] = None
+    units_reported: Optional[str] = None
     type: Optional[Type] = None
     description: Optional[str] = None
     writable: Optional[bool] = None
@@ -58,4 +60,6 @@ class PointPointsetModel(DataModel):
     adjunct: Optional[Dict[str, str]] = None
     tags: Optional[List[Tag]] = None
     structure: Optional[Dict[str, RefDiscovery]] = None
-    link: Optional[str] = None
+    expr: Optional[str] = None
+    values: Optional[Dict[str, str]] = None
+    translation: Optional[BuildingTranslation] = None
